a1e31205c74a2e91de37baa8efd1baad56b5672c
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2007-05-23 19:21:05 +0000

    Temporary commit -- code still being written

--- abstractionalgorithms/CFOptimalRefinement.h
+++ abstractionalgorithms/CFOptimalRefinement.h
@@ -13,7 +13,7 @@
 #include <ext/hash_map>
 #include "OpenClosedList.h"
 #include "FPUtil.h"
-#include "graph.h"
+#include "Graph.h"
 
 /** Definitions for node labels */
 enum {
@@ -40,21 +40,21 @@ struct NodeCompare {
 	bool operator()(const GNode &i1, const GNode &i2)
 	{
 		// if f-cost is tied
-		if (fequal(i1.n->getLabelF(kGCost)+i1.n->getLabelF(kHCost),
-							 i2.n->getLabelF(kGCost)+i2.n->getLabelF(kHCost)))
+		if (fequal(i1.n->GetLabelF(kGCost)+i1.n->GetLabelF(kHCost),
+							 i2.n->GetLabelF(kGCost)+i2.n->GetLabelF(kHCost)))
 		{
 			// if both in/not in open list
-			if (i1.n->getLabelL(kInOpenList) == i2.n->getLabelL(kInOpenList))
+			if (i1.n->GetLabelL(kInOpenList) == i2.n->GetLabelL(kInOpenList))
 			{
 				// return true if node1 has lower g-cost
-				return (fless(i1.n->getLabelF(kGCost), i2.n->getLabelF(kGCost)));
+				return (fless(i1.n->GetLabelF(kGCost), i2.n->GetLabelF(kGCost)));
 			}
 			// return true if node1 isn't in open list
-			return (i2.n->getLabelL(kInOpenList));
+			return (i2.n->GetLabelL(kInOpenList));
 		}
 		// return true if node1 has higher f-cost
-		return (fgreater(i1.n->getLabelF(kGCost)+i1.n->getLabelF(kHCost),
-										 i2.n->getLabelF(kGCost)+i2.n->getLabelF(kHCost)));
+		return (fgreater(i1.n->GetLabelF(kGCost)+i1.n->GetLabelF(kHCost),
+										 i2.n->GetLabelF(kGCost)+i2.n->GetLabelF(kHCost)));
 	}
 };
 
@@ -65,5 +65,20 @@ struct NodeHash {
 
 typedef OpenClosedList<GNode, NodeHash, NodeEqual, NodeCompare> PQueue;
 typedef __gnu_cxx::hash_map<uint32_t, GNode> NodeLookupTable;
+
+class CFOptimalRefinement : public SearchAlgorithm {
+public:
+	CFOptimalRefinement();
+	virtual ~CFOptimalRefinement();
+	virtual const char *GetName();
+	virtual path *GetPath(GraphAbstraction *aMap, node *from, node *to, reservationProvider *rp = 0);
+private:
+	PQueue q;
+	node *aStart, *aGoal;
+	node *gStart;
+	GraphAbstraction *absGraph;
+	Graph *g;
+};
+
 
 #endif
