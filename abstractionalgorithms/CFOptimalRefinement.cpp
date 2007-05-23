68241c4595931759c4831e36d8ddce4238b720f8
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2007-05-23 15:46:58 +0000

    Start of CFDP-like algorithm

---
+++ abstractionalgorithms/CFOptimalRefinement.cpp
@@ -0,0 +1,11 @@
+/*
+ *  CFOptimalRefinement.cpp
+ *  hog2
+ *
+ *  Created by Nathan Sturtevant on 5/22/07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "CFOptimalRefinement.h"
+
