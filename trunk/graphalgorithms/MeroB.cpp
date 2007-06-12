9febe0cd88cfc6efcea2b27b4fc040a54509eaed
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2007-06-12 17:46:49 +0000

    Zhifu's code update

--- trunk/graphalgorithms/MeroB.cpp
+++ trunk/graphalgorithms/MeroB.cpp
@@ -2,10 +2,492 @@
  *  MeroB.cpp
  *  hog2
  *
- *  Created by Nathan Sturtevant on 5/29/07.
- *  Copyright 2007 Nathan Sturtevant, University of Alberta. All rights reserved.
+ *  by Zhifu Zhang 
  *
+ *  This code is not optimized for speed, but rather emphasizing on correctness.
  */
 
 #include "MeroB.h"
 
+bool verbose = true;
+
+void MeroB::GetPath(GraphEnvironment *_env, graphState from, graphState to, std::vector<graphState> &thePath) {
+	if(!InitializeSearch(_env,from,to,thePath))
+		return;
+
+	while(!DoSingleSearchStep(thePath)) 
+		{}
+
+	if(thePath.size() > 0)
+		printf("\nNodes expanded=%d, Nodes touched=%d.\n",GetNodesExpanded(),GetNodesTouched());
+}
+
+bool MeroB::InitializeSearch(GraphEnvironment *_env, graphState from, graphState to, std::vector<graphState> &thePath) {
+	env = _env;
+	nodesTouched = nodesExpanded = 0;
+	start = from;
+	goal = to;
+
+	closedList.clear();
+	openQueue.reset();
+	FCache.reset();
+
+	if ((from == UINT32_MAX) || (to == UINT32_MAX) || (from == to))
+	{
+		thePath.resize(0);
+		return false;
+	}
+
+	// step (1)
+	MeroBUtil::SearchNode first(env->HCost(start, goal), 0, start, start);
+	openQueue.Add(first);
+
+	//if(verID == MB_B || verID == MB_BP)
+		F = 0;
+
+	return true;
+}
+
+bool MeroB::DoSingleSearchStep(std::vector<graphState> &thePath) {
+	if(verID == MB_BP) 
+		return DoSingleStepBP(thePath);
+	else if(verID == MB_B)
+		return DoSingleStepB(thePath);
+	else
+		return DoSingleStepA(thePath);
+}
+
+/* The steps refer to the pseudo codes of the corresponding algorithms */
+
+bool MeroB::DoSingleStepA(std::vector<graphState> &thePath) {
+// return false means the search is not finished, true otherwise
+
+	/* step (2) */
+	if (openQueue.size() == 0)
+	{
+		thePath.resize(0); // no path found!
+		closedList.clear();
+		openQueue.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (3) */
+	nodesExpanded++;
+	MeroBUtil::SearchNode topNode = openQueue.Remove();
+	graphState topNodeID = topNode.currNode;
+	closedList[topNodeID] = topNode;
+
+	if(verbose) {
+		printf("Expanding node %d , g=%lf, h=%lf, f=%lf.\n",topNodeID,topNode.gCost,topNode.fCost-topNode.gCost,topNode.fCost);
+	}
+
+	/* step (4) */
+	if (env->GoalTest(topNodeID, goal))
+	{
+		ExtractPathToStart(topNodeID, thePath);
+		closedList.clear();
+		openQueue.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (5), computing gi is delayed */
+	neighbors.resize(0);
+	env->GetSuccessors(topNodeID, neighbors);
+
+	for(unsigned int x = 0; x<neighbors.size(); x++) {
+		nodesTouched++;
+
+		/* step (5) */
+		graphState neighbor = neighbors[x];
+		double edgeWeight = env->GCost(topNodeID,neighbor);
+		double g = topNode.gCost + edgeWeight;
+		double h = env->HCost(neighbor,goal);
+		double f = g + h;
+
+		/* step (6), neither in OPEN nor CLOSED */
+		if(!openQueue.IsIn(MeroBUtil::SearchNode(neighbor)) && closedList.find(neighbor) == closedList.end()) {
+			MeroBUtil::SearchNode n(f,g,neighbor,topNodeID);
+			openQueue.Add(n);
+
+			if(verbose) {
+				printf("Adding node %d to OPEN, g=%lf, h=%lf, f=%lf.\n",neighbor,g,f-g,f);
+			}
+		}
+
+		/* step (7) */
+		else {
+			MeroBUtil::SearchNode neighborNode;
+			if(openQueue.IsIn(MeroBUtil::SearchNode(neighbor))) {
+				neighborNode = openQueue.find(MeroBUtil::SearchNode(neighbor));
+
+				//if(neighborNode.gCost <= g)
+				if(!fgreater(neighborNode.gCost,g))
+					continue;
+				
+				if(verbose) {
+					printf("Adjusting node %d in OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				neighborNode.copy(f,g,neighbor,topNodeID); // parent is changed
+				openQueue.DecreaseKey(neighborNode);  // adjust its position in OPEN
+			}
+			else if(closedList.find(neighbor) != closedList.end()) {
+				neighborNode = closedList.find(neighbor)->second;
+
+				//if(neighborNode.gCost <= g)
+				if(!fgreater(neighborNode.gCost,g))
+					continue;
+
+				if(verbose) {
+					printf("Moving node %d from CLOSED to OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				neighborNode.copy(f,g,neighbor,topNodeID);  // parent is changed
+				closedList.erase(neighbor);  // delete from CLOSED
+
+				openQueue.Add(neighborNode); // add to OPEN
+			}
+
+		}
+	}
+
+	return false;
+}
+
+/* Algorithm B. step (3) is modified from algorithm A.*/
+bool MeroB::DoSingleStepB(std::vector<graphState> &thePath) {
+	/* step (2) */
+	if (openQueue.size() == 0)
+	{
+		thePath.resize(0); // no path found!
+		closedList.clear();
+		openQueue.reset();
+		FCache.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (3) */
+	nodesExpanded++;
+
+	// put those with f < F into cache
+	FCache.reset();
+	while(openQueue.size() > 0) {
+		MeroBUtil::SearchNode tmpNode = openQueue.top();
+		if(fless(tmpNode.fCost, F)) {
+			FCache.Add(openQueue.Remove());
+		}
+		else
+			break;
+	}
+
+	// select the node to expand
+	MeroBUtil::SearchNode topNode;
+	if(FCache.size() > 0) {
+		topNode = FCache.Remove();
+	}
+	else {
+		topNode = openQueue.Remove();
+		F = topNode.fCost; // update F
+
+		if(verbose) {
+			printf("F updated to %lf.\n",F);
+		}
+	}
+
+	// move remaining nodes from FCache back to openQueue
+	while(FCache.size() > 0) {
+		openQueue.Add(FCache.Remove());
+	}
+
+
+	graphState topNodeID = topNode.currNode;
+	closedList[topNodeID] = topNode;
+
+	if(verbose) {
+		printf("Expanding node %d , g=%lf, h=%lf, f=%lf.\n",topNodeID,topNode.gCost,topNode.fCost-topNode.gCost,topNode.fCost);
+	}
+
+	/* step (4) */
+	if (env->GoalTest(topNodeID, goal))
+	{
+		ExtractPathToStart(topNodeID, thePath);
+		closedList.clear();
+		openQueue.reset();
+		FCache.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (5), computing gi is delayed */
+	neighbors.resize(0);
+	env->GetSuccessors(topNodeID, neighbors);
+
+	for(unsigned int x = 0; x<neighbors.size(); x++) {
+		nodesTouched++;
+
+		/* step (5) */
+		graphState neighbor = neighbors[x];
+		double edgeWeight = env->GCost(topNodeID,neighbor);
+		double g = topNode.gCost + edgeWeight;
+		double h = env->HCost(neighbor,goal);
+		double f = g + h;
+
+		/* step (6), neither in OPEN nor CLOSED */
+		if(!openQueue.IsIn(MeroBUtil::SearchNode(neighbor)) && closedList.find(neighbor) == closedList.end() ) {
+			MeroBUtil::SearchNode n(f,g,neighbor,topNodeID);
+			openQueue.Add(n);
+
+			if(verbose) {
+				printf("Adding node %d to OPEN, g=%lf, h=%lf, f=%lf.\n",neighbor,g,f-g,f);
+			}
+		}
+
+		/* step (7) */
+		else {
+			MeroBUtil::SearchNode neighborNode;
+			if(openQueue.IsIn(MeroBUtil::SearchNode(neighbor))) {
+				neighborNode = openQueue.find(MeroBUtil::SearchNode(neighbor));
+
+				//if(neighborNode.gCost <= g)
+				if(!fgreater(neighborNode.gCost,g))
+					continue;
+				
+				if(verbose) {
+					printf("Adjusting node %d in OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				neighborNode.copy(f,g,neighbor,topNodeID); // parent is changed
+				openQueue.DecreaseKey(neighborNode);  // adjust its position in OPEN
+			}
+			else if(closedList.find(neighbor) != closedList.end()) {
+				neighborNode = closedList.find(neighbor)->second;
+
+				//if(neighborNode.gCost <= g)
+				if(!fgreater(neighborNode.gCost,g))
+					continue;
+
+				if(verbose) {
+					printf("Moving node %d from CLOSED to OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				neighborNode.copy(f,g,neighbor,topNodeID);  // parent is changed
+				closedList.erase(neighbor);  // delete from CLOSED
+
+				openQueue.Add(neighborNode); // add to OPEN
+			}
+
+		}
+	}
+
+	return false;
+}
+
+/* Algorithm B' . step (3a) (3b) are inserted into algorithm B. step (7) is also changed, since we need to store the updated h */
+bool MeroB::DoSingleStepBP(std::vector<graphState> &thePath) {
+	/* step (2) */
+	if (openQueue.size() == 0)
+	{
+		thePath.resize(0); // no path found!
+		closedList.clear();
+		openQueue.reset();
+		FCache.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (3) */
+	nodesExpanded++;
+
+	// put those with f < F into cache
+	FCache.reset();
+	while(openQueue.size() > 0) {
+		MeroBUtil::SearchNode tmpNode = openQueue.top();
+		if(fless(tmpNode.fCost , F)) {
+			FCache.Add(openQueue.Remove());
+		}
+		else
+			break;
+	}
+
+	// select the node to expand
+	MeroBUtil::SearchNode topNode;
+	if(FCache.size() > 0) {
+		topNode = FCache.Remove();
+	}
+	else {
+		topNode = openQueue.Remove();
+		F = topNode.fCost; // update F
+
+		if(verbose) {
+			printf("F updated to %lf.\n",F);
+		}
+	}
+
+	// move remaining nodes from FCache back to openQueue
+	while(FCache.size() > 0) {
+		openQueue.Add(FCache.Remove());
+	}
+
+	graphState topNodeID = topNode.currNode;
+	closedList[topNodeID] = topNode;
+
+	if(verbose) {
+		printf("Expanding node %d , g=%lf, h=%lf, f=%lf.\n",topNodeID,topNode.gCost,topNode.fCost-topNode.gCost,topNode.fCost);
+	}
+
+	/* step (4) */
+	if (env->GoalTest(topNodeID, goal))
+	{
+		ExtractPathToStart(topNodeID, thePath);
+		closedList.clear();
+		openQueue.reset();
+		FCache.reset();
+		env = 0;
+		return true;
+	}
+
+	/* step (5), computing gi is delayed */
+	neighbors.resize(0);
+	env->GetSuccessors(topNodeID, neighbors);
+
+	double hTop = topNode.fCost - topNode.gCost;
+	double minH2 = DBL_MAX; // min ( edgeWeight(i) + h(neighbor(i)) )
+
+	for(unsigned int x = 0; x<neighbors.size(); x++) {
+		nodesTouched++;
+
+		/* step (5) */
+		graphState neighbor = neighbors[x];
+		double edgeWeight = env->GCost(topNodeID,neighbor);
+		double g = topNode.gCost + edgeWeight;
+
+		/* step Mero (3a) */
+		double h_tmp; // for printing reports only
+		double h;
+		// this is necessary because the modified h is stored in the node, not the environment
+		if(openQueue.IsIn(MeroBUtil::SearchNode(neighbor))) { 
+			MeroBUtil::SearchNode nb = openQueue.find(MeroBUtil::SearchNode(neighbor));
+			h = max( nb.fCost - nb.gCost, hTop - edgeWeight);
+
+			h_tmp = nb.fCost - nb.gCost;
+		}
+		else if(closedList.find(neighbor) != closedList.end()) {
+			MeroBUtil::SearchNode nb = closedList.find(neighbor)->second;
+			h = max( nb.fCost - nb.gCost, hTop - edgeWeight);
+
+			h_tmp = nb.fCost - nb.gCost;
+		}
+		else {
+			h = max( env->HCost(neighbor,goal), hTop - edgeWeight);
+
+			h_tmp = env->HCost(neighbor,goal);
+		}
+
+		if(verbose) {
+			if(fgreater(h,h_tmp))
+				printf("Improving h of node %d by Mero rule (a), %lf->%lf\n",neighbor,h_tmp,h);
+		}
+		
+		double f = g + h;
+
+		/* step Mero (3b) */
+		minH2 = min(minH2, h + edgeWeight);
+
+		/* step (6), neither in OPEN nor CLOSED */
+		if(!openQueue.IsIn(MeroBUtil::SearchNode(neighbor)) && closedList.find(neighbor) == closedList.end() ) {
+			MeroBUtil::SearchNode n(f,g,neighbor,topNodeID);
+			openQueue.Add(n);
+
+			if(verbose) {
+				printf("Adding node %d to OPEN, g=%lf, h=%lf, f=%lf.\n",neighbor,g,f-g,f);
+			}
+		}
+
+		/* step (7) */
+		else {
+			MeroBUtil::SearchNode neighborNode;
+			if(openQueue.IsIn(MeroBUtil::SearchNode(neighbor))) {
+				neighborNode = openQueue.find(MeroBUtil::SearchNode(neighbor));
+
+				//if(neighborNode.gCost <= g) {
+				if(!fgreater(neighborNode.gCost,g)) {
+					if(fgreater(h , neighborNode.fCost - neighborNode.gCost)) {
+					// we may fail to update g, but still update h
+						f = h + neighborNode.gCost;
+						neighborNode.fCost = f;
+						openQueue.IncreaseKey(neighborNode);
+					}
+					continue;
+				}
+				
+				if(verbose) {
+					printf("Adjusting node %d in OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				if(fless(f , neighborNode.fCost)) {
+					neighborNode.copy(f,g,neighbor,topNodeID); // parent is changed
+					openQueue.DecreaseKey(neighborNode);  // adjust its position in OPEN
+				}
+				else {
+					neighborNode.copy(f,g,neighbor,topNodeID); // parent is changed
+					openQueue.IncreaseKey(neighborNode);  // adjust its position in OPEN
+				}
+			}
+			else if(closedList.find(neighbor) != closedList.end()) {
+				neighborNode = closedList.find(neighbor)->second;
+
+				//if(neighborNode.gCost <= g) {
+				if(!fgreater(neighborNode.gCost,g)) {
+					if(fgreater(h , neighborNode.fCost - neighborNode.gCost)) {
+					// we may fail to update g, but still update h
+						f = h + neighborNode.gCost;
+						neighborNode.fCost = f;
+						closedList[neighbor] = neighborNode;
+					}
+					continue;
+				}
+
+				if(verbose) {
+					printf("Moving node %d from CLOSED to OPEN, g=%lf, h=%lf, f=%lf; g_old=%lf.\n",neighbor,g,f-g,f, neighborNode.gCost);
+				}
+
+				neighborNode.copy(f,g,neighbor,topNodeID);  // parent is changed
+				closedList.erase(neighbor);  // delete from CLOSED
+
+				openQueue.Add(neighborNode); // add to OPEN
+			}
+
+		}
+	}
+
+	/* step Mero (3b), update h of parent */
+	if(fgreater(minH2 , hTop)) {
+		topNode.fCost = minH2 + topNode.gCost;  // f = h + g
+		closedList[topNodeID] = topNode;
+
+		if(verbose) {
+			printf("Improving h of node %d by Mero rule (b), %lf->%lf\n",topNodeID,hTop,minH2);
+		}
+	}
+
+	return false;
+}
+
+void MeroB::ExtractPathToStart(graphState goalNode, std::vector<graphState> &thePath)
+{
+	MeroBUtil::SearchNode n;
+	if (closedList.find(goalNode) != closedList.end())
+	{
+		n = closedList[goalNode];
+	}
+	else n = openQueue.find(MeroBUtil::SearchNode(goalNode));
+
+	do {
+		thePath.push_back(n.currNode);
+		n = closedList[n.prevNode];
+	} while (n.currNode != n.prevNode);
+	//thePath.push_back(n.currNode);
+}
--- trunk/graphalgorithms/MeroB.h
+++ trunk/graphalgorithms/MeroB.h
@@ -2,31 +2,221 @@
  *  MeroB.h
  *  hog2
  *
- *  Created by Nathan Sturtevant on 5/29/07.
- *  Copyright 2007 Nathan Sturtevant, University of Alberta. All rights reserved.
+ *  Created by Nathan Sturtevant, modified by Zhifu Zhang
  *
  */
 
 #ifndef MEROB_H
 #define MEROB_H
 
+#include <math.h>
 #include "SearchEnvironment.h"
 #include "GraphEnvironment.h"
 #include <ext/hash_map>
 #include "FPUtil.h"
+#include "OpenClosedList.h"
 
-typedef __gnu_cxx::hash_map<uint64_t, double> NodeHashTable;
+#ifndef UINT32_MAX
+#define UINT32_MAX        4294967295U
+#endif
+
+
+#define MB_A 1
+#define MB_B 2
+#define MB_BP 3
+
+//typedef __gnu_cxx::hash_map<uint64_t, double> NodeHashTable;
+
+namespace MeroBUtil
+{
+	class SearchNode {
+	public:
+		SearchNode(double _fCost=0, double _gCost=0, graphState curr=0, graphState prev=0)
+		:fCost(_fCost), gCost(_gCost), currNode(curr), prevNode(prev) {}
+
+		SearchNode(graphState curr)
+		:fCost(0), gCost(0), currNode(curr), prevNode(0) {}
+
+		void copy(double f, double g, graphState curr, graphState prev) {
+			fCost = f;
+			gCost = g;
+			currNode = curr;
+			prevNode = prev;
+		}
+
+		double fCost;
+		double gCost;
+		graphState currNode;
+		graphState prevNode;
+	};
+
+	struct SearchNodeEqual {
+		bool operator()(const SearchNode &i1, const SearchNode &i2)
+		{ return (i1.currNode == i2.currNode); } 
+	};
+
+	struct SearchNodeCompare { // true means i2 is preferable over i1
+		// prefering larger g, i.e. smaller h is also in favor of goal nodes
+		bool operator()(const SearchNode &i1, const SearchNode &i2)
+		{
+			if (fequal(i1.fCost, i2.fCost))
+			{
+				return (fless(i1.gCost, i2.gCost));
+			}
+			return (fgreater(i1.fCost, i2.fCost));
+		} 
+	};
+
+	struct GGreater {
+		bool operator()(const SearchNode &i1, const SearchNode &i2) {
+			if(fequal(i1.gCost,i2.gCost))
+				return fgreater(i1.fCost,i2.fCost);
+
+			return fgreater(i1.gCost,i2.gCost);
+		}
+	};
+
+	struct SearchNodeHash {
+		size_t operator()(const SearchNode &x) const
+		{ return (size_t)(x.currNode); }
+	};
+
+	struct graphGenerator {
+		static Graph* genFig1(int N) {
+			// h(0) = h(1) = 0; 
+			// h(i) = 2^(i-1) + 2i - 3, for 1<i<=N
+			// c(i,j) = 2^(i-2) + i - 2^(j-1) - j, for 1<=j<i<=N
+			// c(1,0) = 2^(N-1) + N - 2
+			assert(N >= 3);
+
+			Graph *g = new Graph();
+
+			// add nodes
+			unsigned int nodeID = 0;
+			for(;nodeID<=N;nodeID++) {
+				node *n = new node("");
+				if(nodeID == 0 || nodeID == 1) {
+					n->SetLabelF(GraphSearchConstants::kHCost,0);
+				}
+				else {
+					double h = pow(2,nodeID-1) + 2*nodeID - 3;
+					n->SetLabelF(GraphSearchConstants::kHCost,h);
+				}
+				g->AddNode(n); // the real nodeID is assigned here
+			}
+
+			// add edges
+			double c = pow(2,N-1) + N - 2;
+			edge *e = new edge(1,0,c);
+			g->AddEdge(e);
+			for(int j=1;j<N;j++) {
+				for(int i=j+1;i<=N;i++) {
+					c = pow(2,i-2) + i - pow(2,j-1) - j;
+					e = new edge(i,j,c);
+					g->AddEdge(e);
+				}
+			}
+
+			return g;
+		}
+
+		static Graph* genFig2(int N) {
+			// h(0) = h(2N-1) = 0
+			// h(i) = 2(N-1)^2 - N - i + 2, for i = 1,...,N-1
+			// h(N+j) = 2(N-1)(N-2-j) + 1,  for j = 0,...,N-2
+			// c(0,i) = 1,  for i = 1,...,N-1
+			// c(i,N) = 2(N-1)(i-1) + 1,  for i = 1,...,N-1
+			// c(i,i+1) = 2N - 2,  for i = N,...,2N-1 (should it be 2N-2 ?)
+			assert(N >= 2);
+
+			Graph* g = new Graph();
+
+			// add nodes
+			node* n = new node("");
+			n->SetLabelF(GraphSearchConstants::kHCost,0);
+			g->AddNode(n);
+
+			for(unsigned int i=1;i<=N-1;i++) {
+				n = new node("");
+				double h = 2*(N-1)*(N-1) - N - i + 2;
+				n->SetLabelF(GraphSearchConstants::kHCost,h);
+				g->AddNode(n);
+			}
+
+			for(unsigned int j=0;j<=N-2;j++) {
+				n = new node("");
+				double h = 2*(N-1)*(N-2-j) + 1;
+				n->SetLabelF(GraphSearchConstants::kHCost,h);
+				g->AddNode(n);
+			}
+
+			n = new node(""); // the last node (2N-1)
+			n->SetLabelF(GraphSearchConstants::kHCost,0);
+			g->AddNode(n);
+
+			// add edges
+
+			// type 1
+			for(unsigned int i=1;i<=N-1;i++) {
+				edge* e = new edge(0,i,1);
+				g->AddEdge(e);
+			}
+
+			// type 2
+			for(unsigned int i=1;i<=N-1;i++) {
+				double c = 2*(N-1)*(i-1) + 1;
+				edge* e = new edge(i,N,c);
+				g->AddEdge(e);
+			}
+
+			// type 3
+			for(unsigned int i=N;i<=2*N-2;i++) {
+				double c = 2*N - 2;
+				edge* e = new edge(i,i+1,c);
+				g->AddEdge(e);
+			}
+
+			return g;
+		}
+	};
+
+	typedef OpenClosedList<MeroBUtil::SearchNode, MeroBUtil::SearchNodeHash,
+		MeroBUtil::SearchNodeEqual, MeroBUtil::SearchNodeCompare> PQueue;
+
+	typedef __gnu_cxx::hash_map<graphState, MeroBUtil::SearchNode > NodeLookupTable;
+
+	typedef OpenClosedList<MeroBUtil::SearchNode, MeroBUtil::SearchNodeHash,
+		MeroBUtil::SearchNodeEqual, MeroBUtil::GGreater> GQueue;
+}
 
 class MeroB {
 public:
-	MeroB() { }
+	MeroB() { verID = MB_A;}
+	MeroB(unsigned int v) { verID = v; }
 	virtual ~MeroB() {}
 	void GetPath(GraphEnvironment *env, graphState from, graphState to, std::vector<graphState> &thePath);
 	
 	long GetNodesExpanded() { return nodesExpanded; }
 	long GetNodesTouched() { return nodesTouched; }
+
+	bool InitializeSearch(GraphEnvironment *env, graphState from, graphState to, std::vector<graphState> &thePath);
+	bool DoSingleSearchStep(std::vector<graphState> &thePath);
+	bool DoSingleStepA(std::vector<graphState> &thePath);
+	bool DoSingleStepB(std::vector<graphState> &thePath);
+	bool DoSingleStepBP(std::vector<graphState> &thePath);
+	void ExtractPathToStart(graphState goalNode, std::vector<graphState> &thePath);
+
 private:
+	unsigned int verID;
+	double F;
 	long nodesExpanded, nodesTouched;
+	std::vector<graphState> neighbors;
+	graphState goal, start;
+	GraphEnvironment *env;
+	MeroBUtil::PQueue openQueue;
+	MeroBUtil::NodeLookupTable closedList; 
+	MeroBUtil::GQueue FCache; // storing nodes with f < F, this is temporary cache
+
 };	
 
 #endif
