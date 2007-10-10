3ea2679079568d90fa745d55a22e488ff6c457b8
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2007-10-10 00:01:54 +0000

    Simple RRAStar implementation of Hierarchical A Star.

---
+++ trunk/abstractionalgorithms/SimpleHierarchicalAStar.h
@@ -0,0 +1,55 @@
+/*
+ *  SimpleHierarchicalAStar.h
+ *  hog2
+ *
+ *  Created by Nathan Sturtevant on 10/9/07.
+ *  Copyright 2007 Nathan Sturtevant, University of Alberta. All rights reserved.
+ *
+ */
+
+#ifndef SIMPLEHIERARCHICALASTAR_H
+#define SIMPLEHIERARCHICALASTAR_H
+
+#include "TemplateAStar.h"
+#include "SearchEnvironment.h"
+
+class HierarchicalHeuristicEnvironment : public SearchEnvironment<unsigned long, unsigned long>
+{
+	virtual ~HierarchicalHeuristicEnvironment() {}
+	virtual void GetSuccessors(unsigned long &nodeID, std::vector<unsigned long> &neighbors) = 0;
+	virtual void GetActions(unsigned long &nodeID, std::vector<unsigned long> &actions) = 0;
+	virtual unsigned long GetAction(unsigned long &s1, unsigned long &s2) = 0;
+	virtual void ApplyAction(unsigned long &s, unsigned long a) = 0;
+	
+	virtual void GetNextState(unsigned long &currents, unsigned long dir, unsigned long &news){};
+	
+	virtual bool InvertAction(unsigned long &a) = 0;
+	
+	virtual double HCost(unsigned long &node1, unsigned long &node2) = 0;
+	virtual double GCost(unsigned long &node1, unsigned long &node2) = 0;
+	virtual bool GoalTest(unsigned long &node, unsigned long &goal) = 0;
+	
+	virtual uint64_t GetStateHash(unsigned long &node) = 0;
+	virtual uint64_t GetActionHash(unsigned long act) = 0;
+	
+	virtual OccupancyInterface<unsigned long,unsigned long> *GetOccupancyInfo() = 0;
+	
+	virtual void OpenGLDraw(int window) = 0;
+	virtual void OpenGLDraw(int window, unsigned long&) = 0;
+	virtual void OpenGLDraw(int window, unsigned long&, unsigned long&) = 0;
+};
+
+class SimpleHierarchicalAStar : public SearchAlgorithm {
+public:
+	SimpleHierarchicalAStar();
+	virtual ~SimpleHierarchicalAStar();
+	virtual const char *GetName() { return "SimpleHierarchicalAStar"; }
+	virtual path *GetPath(GraphAbstraction *aMap, node *from, node *to, reservationProvider *rp = 0);
+	path *DoOneSearchStep();
+	bool InitializeSearch(GraphAbstraction *aMap, node *from, node *to);
+	void OpenGLDraw();
+private:	
+TemplateAStar<unsigned long, unsigned long, 
+};
+
+#endif
