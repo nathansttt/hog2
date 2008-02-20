2b16beb9b51519d36d872620bdb01b5bb98632c7
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2008-02-20 19:05:48 +0000

    old changes

--- generic/IDAStar.h
+++ generic/IDAStar.h
@@ -10,6 +10,7 @@
 #ifndef IDASTAR_H
 #define IDASTAR_H
 
+#include <iostream>
 #include "SearchEnvironment.h"
 #include <ext/hash_map>
 #include "FPUtil.h"
@@ -23,7 +24,9 @@ public:
 	virtual ~IDAStar() {}
 	void GetPath(SearchEnvironment<state, action> *env, state from, state to,
 							 std::vector<state> &thePath);
-	
+	void GetPath(SearchEnvironment<state, action> *env, state from, state to,
+				 std::vector<action> &thePath);
+
 	long GetNodesExpanded() { return nodesExpanded; }
 	long GetNodesTouched() { return nodesTouched; }
 	void ResetNodeCount() { nodesExpanded = nodesTouched = 0; }
@@ -32,10 +35,16 @@ private:
 	unsigned long nodesExpanded, nodesTouched;
 	
 	double DoIteration(SearchEnvironment<state, action> *env,
-										 state parent, state currState, state goal,
-										 std::vector<state> &thePath, double bound, double g,
-										 double maxH);
+					   state parent, state currState,
+					   std::vector<state> &thePath, double bound, double g,
+					   double maxH);
+	double DoIteration(SearchEnvironment<state, action> *env,
+					   action forbiddenAction, state &currState,
+					   std::vector<action> &thePath, double bound, double g,
+					   double maxH);
+	
 	void UpdateNextBound(double currBound, double fCost);
+	state goal;
 	double nextBound;
 	NodeHashTable nodeTable;
 	bool usePathMax;
@@ -44,26 +53,47 @@ private:
 
 template <class state, class action>
 void IDAStar<state, action>::GetPath(SearchEnvironment<state, action> *env,
-																		 state from, state to,
-																		 std::vector<state> &thePath)
+									 state from, state to,
+									 std::vector<state> &thePath)
 {
 	nextBound = 0;
 	nodesExpanded = nodesTouched = 0;
 	thePath.resize(0);
 	UpdateNextBound(0, env->HCost(from, to));
+	goal = to;
 	while (thePath.size() == 0)
 	{
 		nodeTable.clear();
 		printf("Starting iteration with bound %f\n", nextBound);
-		DoIteration(env, from, from, to, thePath, nextBound, 0, 0);
+		DoIteration(env, from, from, thePath, nextBound, 0, 0);
+	}
+}
+
+template <class state, class action>
+void IDAStar<state, action>::GetPath(SearchEnvironment<state, action> *env,
+									 state from, state to,
+									 std::vector<action> &thePath)
+{
+	nextBound = 0;
+	nodesExpanded = nodesTouched = 0;
+	thePath.resize(0);
+	UpdateNextBound(0, env->HCost(from, to));
+	goal = to;
+	std::vector<action> act;
+	env->GetActions(from, act);
+	while (thePath.size() == 0)
+	{
+		nodeTable.clear();
+		printf("Starting iteration with bound %f\n", nextBound);
+		DoIteration(env, act[0], from, thePath, nextBound, 0, 0);
 	}
 }
 
 template <class state, class action>
 double IDAStar<state, action>::DoIteration(SearchEnvironment<state, action> *env,
-																					 state parent, state currState, state goal,
-																					 std::vector<state> &thePath, double bound, double g,
-																					 double maxH)
+										   state parent, state currState,
+										   std::vector<state> &thePath, double bound, double g,
+										   double maxH)
 {
 	nodesExpanded++;
 	double h = env->HCost(currState, goal);
@@ -77,17 +107,8 @@ double IDAStar<state, action>::DoIteration(SearchEnvironment<state, action> *env
 		//printf("Stopping at (%d, %d). g=%f h=%f\n", currState>>16, currState&0xFFFF, g, h);
 		return h;
 	}
-// FIXME -- hash table
-//	if (nodeTable.find(currState) != nodeTable.end()) // already seen
-//	{
-//		if (fless(g/*+h*/, nodeTable[currState])) // with lower g-cost
-//		{
-//		}
-//		else {
-//			return h;
-//		}
-//	}
-//	nodeTable[currState] = g;//+h;
+	if (env->GoalTest(currState, goal))
+		return 0;
 		
 	std::vector<state> neighbors;
 	env->GetSuccessors(currState, neighbors);
@@ -99,9 +120,9 @@ double IDAStar<state, action>::DoIteration(SearchEnvironment<state, action> *env
 			continue;
 		thePath.push_back(neighbors[x]);
 		double edgeCost = env->GCost(currState, neighbors[x]);
-		double childH = DoIteration(env, currState, neighbors[x], goal, thePath, bound,
+		double childH = DoIteration(env, currState, neighbors[x], thePath, bound,
 																g+edgeCost, maxH - edgeCost);
-		if (thePath.back() == goal)
+		if (env->GoalTest(thePath.back(), goal))
 			return 0;
 		thePath.pop_back();
 		// pathmax
@@ -120,12 +141,78 @@ double IDAStar<state, action>::DoIteration(SearchEnvironment<state, action> *env
 }
 
 template <class state, class action>
+double IDAStar<state, action>::DoIteration(SearchEnvironment<state, action> *env,
+										   action forbiddenAction, state &currState,
+										   std::vector<action> &thePath, double bound, double g,
+										   double maxH)
+{
+	nodesExpanded++;
+	double h = env->HCost(currState, goal);
+	
+	// path max
+	if (usePathMax && fless(h, maxH))
+		h = maxH;
+	if (fgreater(g+h, bound))
+	{
+		UpdateNextBound(bound, g+h);
+		//printf("Stopping at (%d, %d). g=%f h=%f\n", currState>>16, currState&0xFFFF, g, h);
+		return h;
+	}
+	if (env->GoalTest(currState, goal))
+		return -1; // found goal
+	
+	std::vector<action> actions;
+	env->GetActions(currState, actions);
+	nodesTouched += actions.size();
+	int depth = thePath.size();
+	
+	for (unsigned int x = 0; x < actions.size(); x++)
+	{
+		if ((depth != 0) && (actions[x] == forbiddenAction))
+			continue;
+
+		thePath.push_back(actions[x]);
+
+		double edgeCost = env->GCost(currState, actions[x]);
+		env->ApplyAction(currState, actions[x]);
+		env->InvertAction(actions[x]);
+		double childH = DoIteration(env, actions[x], currState, thePath, bound,
+									g+edgeCost, maxH - edgeCost);
+		env->ApplyAction(currState, actions[x]);
+		if (fequal(childH, -1)) // found goal
+			return -1;
+
+		thePath.pop_back();
+
+		// pathmax
+		if (usePathMax && fgreater(childH-edgeCost, h))
+		{
+			//			nodeTable[currState] = g;//+h
+			h = childH-edgeCost;
+			if (fgreater(g+h, bound))
+			{
+				UpdateNextBound(bound, g+h);
+				return h;
+			}
+		}
+	}
+	return h;
+}
+
+
+template <class state, class action>
 void IDAStar<state, action>::UpdateNextBound(double currBound, double fCost)
 {
 	if (!fgreater(nextBound, currBound))
+	{
 		nextBound = fCost;
+		printf("Updating next bound to %f\n", nextBound);
+	}
 	else if (fgreater(fCost, currBound) && fless(fCost, nextBound))
+	{
 		nextBound = fCost;
+		printf("Updating next bound to %f\n", nextBound);
+	}
 }
 
 
