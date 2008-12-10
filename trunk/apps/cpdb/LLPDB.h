0b92dd617db02bb54671ba43a83a59fd4ce6f8ff
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2008-12-10 06:20:12 +0000

    moving files around

---
+++ trunk/apps/cpdb/LLPDB.h
@@ -0,0 +1,30 @@
+/*
+ *  LLPDB.h
+ *  hog2
+ *
+ *  Created by Nathan Sturtevant on 9/18/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ * This file is part of HOG.
+ *
+ * HOG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * HOG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with HOG; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef LLPDB_H
+#define LLPDB_H
+
+
+#endif
