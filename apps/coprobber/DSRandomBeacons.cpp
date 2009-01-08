cd1a1b6150b11ea9200856dfebdc116657da63db
carsten.moldenhauer <carsten.moldenhauer@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2009-01-08 23:11:17 +0000

    added target with random beacons added DSRandomBeacons to test suite

---
+++ apps/coprobber/DSRandomBeacons.cpp
@@ -0,0 +1,63 @@
+#include "DSRandomBeacons.h"
+
+DSRandomBeacons::DSRandomBeacons( MapAbstraction *_mabs, bool canPass, unsigned int cop_speed ):
+	mabs(_mabs),
+	gh( new MaximumNormAbstractGraphMapHeuristic( mabs->GetAbstractGraph(0), mabs->GetMap() ) ),
+	env( new GraphEnvironment( mabs->GetAbstractGraph(0), gh ) ),
+	dscrenv( new DSCREnvironment<graphState,graphMove>( env, canPass, cop_speed ) ),
+	pra( new praStar() )
+{
+	// initialize the random number generator
+	//time_t t; time( &t ); srandom( (unsigned int) t );
+};
+
+DSRandomBeacons::~DSRandomBeacons() {
+	delete dscrenv;
+	delete env;
+	delete gh;
+	delete pra;
+};
+
+void DSRandomBeacons::GetPath( graphState pos_robber, graphState pos_cop, unsigned int num_beacons, std::vector<graphState> &resultpath ) {
+
+	resultpath.clear();
+	node *target = NULL;
+	double target_h = DBL_MIN;
+
+	Graph *g = mabs->GetAbstractGraph(0);
+	node *probber = g->GetNode( pos_robber );
+
+	for( unsigned int i = 0; i < num_beacons; i++ ) {
+		node *r = g->GetRandomNode();
+		graphState rs = r->GetNum();
+		double r_h = dscrenv->HCost( rs, pos_cop );
+		//printf( "testing node %d: %g\n", r->GetNum(), r_h );
+		
+		if( mabs->Pathable(probber,r) && (target == NULL || fgreater(r_h, target_h)) ) {
+			target = r;
+			target_h = r_h;
+		}
+	}
+
+	if( target == NULL || probber == target ) {
+		// if we didn't find a new target at all, i.e. we couldn't find a node
+		// to run because all random nodes weren't pathable, stay where you are
+		resultpath.push_back( pos_robber );
+	} else {
+		// generate a PRA* path to this target
+		path *p = pra->GetPath( mabs, probber, target );
+		// transform the path into a vector
+		path *trav = p->next;
+		while( trav ) {
+			resultpath.push_back( trav->n->GetNum() );
+			trav = trav->next;
+		}
+		// cleanup
+		delete p;
+	}
+
+	return;
+};
+
+
+
---
+++ apps/coprobber/DSRandomBeacons.h
@@ -0,0 +1,42 @@
+#include <vector>
+#include "GraphAbstraction.h"
+#include "GraphEnvironment.h"
+#include "DSCREnvironment.h"
+#include "MaximumNormAbstractGraphMapHeuristic.h"
+#include "PRAStar.h"
+
+#ifndef DSRANDOMBEACONS_H
+#define DSRANDOMBEACONS_H
+
+/*
+	Implentation of a strategy that randomly places beacons on the map
+	and then evaluates these beacons heuristically. It then produces
+	a path towards the beacon with the best evaluate (via PRA*).
+*/
+class DSRandomBeacons {
+
+	public:
+
+	// constructor & destructor
+	DSRandomBeacons( MapAbstraction *mabs, bool canPass = true,
+		unsigned int cop_speed = 1 );
+	virtual ~DSRandomBeacons();
+
+	// this algorithm only functions for the robber and produces a path
+	// that he should take
+	void GetPath( graphState pos_robber, graphState pos_cop, unsigned int num_beacons,
+		std::vector<graphState> &path );
+
+	protected:
+
+	MapAbstraction *mabs;
+	MaximumNormAbstractGraphMapHeuristic *gh;
+	GraphEnvironment *env;
+	// we need this for heuristic evaluation
+	DSCREnvironment<graphState,graphMove> *dscrenv;
+	praStar *pra;
+
+};
+
+
+#endif
