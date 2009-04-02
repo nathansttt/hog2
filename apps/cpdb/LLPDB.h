cd19a95afc9ca66c2d708325f28d7f68f8bcd418
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2009-04-02 16:05:34 +0000

    modified to compile properly under gcc

--- apps/cpdb/LLPDB.h
+++ apps/cpdb/LLPDB.h
@@ -26,5 +26,16 @@
 #ifndef LLPDB_H
 #define LLPDB_H
 
+#include "Common.h"
+
+void MyWindowHandler(unsigned long windowID, tWindowEventType eType);
+void MyFrameHandler(unsigned long windowID, unsigned int viewport, void *data);
+void MyDisplayHandler(unsigned long windowID, tKeyboardModifier, char key);
+void MyPathfindingKeyHandler(unsigned long windowID, tKeyboardModifier, char key);
+void MyRandomUnitKeyHandler(unsigned long windowID, tKeyboardModifier, char key);
+int MyCLHandler(char *argument[], int maxNumArgs);
+bool MyClickHandler(unsigned long windowID, int x, int y, point3d loc, tButtonType, tMouseEventType);
+void InstallHandlers();
+void MyPDBKeyHandler(unsigned long, tKeyboardModifier mod, char key);
 
 #endif
