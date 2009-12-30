3c9cdd56fcad5b9f3c9d4aaca503050b3f6890c3
carsten.moldenhauer <carsten.moldenhauer@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2009-12-30 17:16:08 +0000

    added a few more jumping rules for testing

--- trunk/apps/sfbds/astar.cpp
+++ trunk/apps/sfbds/astar.cpp
@@ -1,27 +1,41 @@
 #include "astar.h"
 
 template<>
+void SFBDSAStar<graphState,graphMove>::OpenGLDraw_RegisterClosedState( QueueNode &q ) {
+	if( OpenGLDrawState == NULL ) {
+		unsigned int num_states = ((GraphEnvironment*)env)->GetGraph()->GetNumNodes();
+		OpenGLDrawState = new bool[num_states];
+		for( unsigned int i = 0; i < num_states; i++ )
+			OpenGLDrawState[i] = false;
+	}
+
+	OpenGLDrawState[q.s1] = true;
+	OpenGLDrawState[q.s2] = true;
+	return;
+}
+
+
+template<>
 void SFBDSAStar<graphState,graphMove>::OpenGLDraw( GLdouble rad ) const {
+
+	if( OpenGLDrawState == NULL ) return;
+
+	unsigned int num_states = ((GraphEnvironment*)env)->GetGraph()->GetNumNodes();
 
 	// draw closed list in green
 	glColor3f(0., 1., 0.);
-	for( AStarClosedList::iterator closedit = closed.begin();
-	     closedit != closed.end(); closedit++ ) {
-		GLdouble x,y,z;
 
-		// draw start state
-		node* n = ((GraphEnvironment*)env)->GetGraph()->GetNode( closedit->s1 );
-		x = n->GetLabelF(GraphSearchConstants::kXCoordinate);
-		y = n->GetLabelF(GraphSearchConstants::kYCoordinate);
-		z = n->GetLabelF(GraphSearchConstants::kZCoordinate);
-		DrawSphere( x, y, z, rad );
+	for( unsigned int i = 0; i < num_states; i++ ) {
+		if( OpenGLDrawState[i] ) {
+			GLdouble x,y,z;
 
-		// draw end state
-		n = ((GraphEnvironment*)env)->GetGraph()->GetNode( closedit->s2 );
-		x = n->GetLabelF(GraphSearchConstants::kXCoordinate);
-		y = n->GetLabelF(GraphSearchConstants::kYCoordinate);
-		z = n->GetLabelF(GraphSearchConstants::kZCoordinate);
-		DrawSphere( x, y, z, rad );
+			// draw start state
+			node* n = ((GraphEnvironment*)env)->GetGraph()->GetNode( i );
+			x = n->GetLabelF(GraphSearchConstants::kXCoordinate);
+			y = n->GetLabelF(GraphSearchConstants::kYCoordinate);
+			z = n->GetLabelF(GraphSearchConstants::kZCoordinate);
+			DrawSphere( x, y, z, rad );
+		}
 	}
 
 	return;
