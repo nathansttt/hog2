de0f622dd476aafc8b42068027577e545e507a81
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2014-01-26 08:21:23 +0000

    new bloom

---
+++ trunk/utils/Bloom.h
@@ -0,0 +1,31 @@
+//
+//  Bloom.h
+//  hog2 glut
+//
+//  Created by Nathan Sturtevant on 1/25/14.
+//  Copyright (c) 2014 University of Denver. All rights reserved.
+//
+
+#ifndef __hog2_glut__Bloom__
+#define __hog2_glut__Bloom__
+
+#include <iostream>
+#include <stdint.h>
+#include "BitVector.h"
+
+class BloomFilter {
+public:
+	BloomFilter(uint64_t numItems, double targetHitRate, bool save, bool zero=true);
+	~BloomFilter();
+	void Insert(uint64_t item);
+	bool Contains(uint64_t item);
+	uint64_t GetStorage() { return filterSize; }
+	int GetNumHash() { return numHash; }
+private:
+	uint64_t Hash(uint64_t value, int which);
+	int numHash;
+	uint64_t filterSize;
+	BitVector *bits;
+};
+
+#endif /* defined(__hog2_glut__Bloom__) */
