72dbbd4c7e32ef9d13ee317b94504d71cc440555
nathanst <nathanst@3e291e08-dd2d-0410-9de5-7df0620fb8d4>
2014-08-07 05:08:00 +0000

    Improved code for demonstrating and visualizing learning approaches

--- apps/supervisedLearning/Sample.cpp
+++ apps/supervisedLearning/Sample.cpp
@@ -27,19 +27,14 @@
 
 #include "Common.h"
 #include "Sample.h"
-//#include "UnitSimulation.h"
-//#include "EpisodicSimulation.h"
-//#include "Map2DEnvironment.h"
-//#include "RandomUnits.h"
-//#include "AStar.h"
-//#include "TemplateAStar.h"
-//#include "GraphEnvironment.h"
-//#include "MapSectorAbstraction.h"
-//#include "GraphRefinementEnvironment.h"
 #include "LinearRegression.h"
+#include "LogisticRegression.h"
 #include "NN.h"
 
-#define CLASSIFIER
+bool CLASSIFIER = 0;
+bool NN = false;
+//#define KERNEL
+const double scale = 2.0;
 
 struct data
 {
@@ -49,9 +44,16 @@ struct data
 
 std::vector<data> points;
 
+FunctionApproximator *cl = new LinearRegression(3, 1, 0.01);
+FunctionApproximator *l = new LinearRegression(2, 1, 0.01);
+
 //LinearRegression l(1, 1, 0.01);
-NN l(1, 10, 1, 0.1);
-LinearRegression cl(2, 1, 0.01);
+//NN l(1, 10, 1, 0.1);
+
+//NN cl(2, 10, 1, 0.01);
+//LinearRegression cl(3, 1, 0.01);
+
+bool threshold = false;
 
 int main(int argc, char* argv[])
 {
@@ -66,13 +68,13 @@ int main(int argc, char* argv[])
 void InstallHandlers()
 {
 	InstallKeyboardHandler(MyDisplayHandler, "Toggle Abstraction", "Toggle display of the ith level of the abstraction", kAnyModifier, '0', '9');
-	InstallKeyboardHandler(MyDisplayHandler, "Cycle Abs. Display", "Cycle which group abstraction is drawn", kAnyModifier, '\t');
+	InstallKeyboardHandler(MyDisplayHandler, "Cycle Abs. Display", "Cycle which group abstraction is drawn", kAnyModifier, 't');
+	InstallKeyboardHandler(MyDisplayHandler, "Toggle classifier", "toggle classifier", kAnyModifier, 'c');
+	InstallKeyboardHandler(MyDisplayHandler, "Toggle Neural Network", "toggle Neural Network", kAnyModifier, 'n');
 	InstallKeyboardHandler(MyDisplayHandler, "Pause Simulation", "Pause simulation execution.", kNoModifier, 'p');
 	InstallKeyboardHandler(MyDisplayHandler, "Step Simulation", "If the simulation is paused, step forward .1 sec.", kNoModifier, 'o');
-	InstallKeyboardHandler(MyDisplayHandler, "Step History", "If the simulation is paused, step forward .1 sec in history", kAnyModifier, '}');
-	InstallKeyboardHandler(MyDisplayHandler, "Step History", "If the simulation is paused, step back .1 sec in history", kAnyModifier, '{');
-	InstallKeyboardHandler(MyDisplayHandler, "Step Abs Type", "Increase abstraction type", kAnyModifier, ']');
-	InstallKeyboardHandler(MyDisplayHandler, "Step Abs Type", "Decrease abstraction type", kAnyModifier, '[');
+	InstallKeyboardHandler(MyDisplayHandler, "Toggle threshold", "Toggle whether output is thresholded for display.", kAnyModifier, ']');
+	InstallKeyboardHandler(MyDisplayHandler, "Toggle threshold", "Toggle whether output is thresholded for display.", kAnyModifier, '[');
 	
 	InstallKeyboardHandler(MyPathfindingKeyHandler, "Mapbuilding Unit", "Deploy unit that paths to a target, building a map as it travels", kNoModifier, 'd');
 	InstallKeyboardHandler(MyRandomUnitKeyHandler, "Add A* Unit", "Deploys a simple a* unit", kNoModifier, 'a');
@@ -127,42 +129,48 @@ void MyFrameHandler(unsigned long windowID, unsigned int viewport, void *)
 	}
 	glEnd();
 	
-#ifndef CLASSIFIER
-	glBegin(GL_LINE_STRIP);
-	for (double x = -1; x <= 1; x += 1.0/64.0)
+	if (!CLASSIFIER)
 	{
-//		if (random()%2)
-//			glColor3f(1, 1, 1);
-//		else
-			glColor3f(1, 1, 0);
-		std::vector<double> in(1);
-		in[0] = x;
-		double res = *l.test(in);
-		res = (res-0.5)*5;
-		glVertex3f(x, res, 0);
-	}
-	glEnd();
-#else
-	glPointSize(4.0);
-	glBegin(GL_POINTS);
-	std::vector<double> in(2);
-	for (double x = -1; x <= 1; x += 0.03125*0.5)
-	{
-		for (double y = -1; y <= 1; y += 0.03125*0.5)
+		glBegin(GL_LINE_STRIP);
+		for (double x = -1; x <= 1; x += 1.0/64.0)
 		{
+			glColor3f(1, 1, 0);
+			std::vector<double> in(2);
 			in[0] = x;
-			in[1] = y;
-			double res = *cl.test(in);
-//			if (res > 0.5)
-//				glColor4f(0, 1, 0, 0.5);
-//			else
-//				glColor4f(1, 0, 0, 0.5);
-			glColor3f((1-res)*(1-res), res*res, 0);
-			glVertex3f(x, y, 0);
+			in[1] = x*x;
+			double res = *l->test(in);
+			res = (res-0.5)*scale;
+			glVertex3f(x, res, 0);
 		}
+		glEnd();
 	}
-	glEnd();
-#endif
+	else {
+		glPointSize(4.0);
+		glBegin(GL_POINTS);
+		std::vector<double> in(3);
+		for (double x = -1; x <= 1; x += 0.03125*0.5)
+		{
+			for (double y = -1; y <= 1; y += 0.03125*0.5)
+			{
+				in[0] = x;
+				in[1] = y;
+				in[2] = x*y;//sqrt(x*x+y*y);//x*y;
+				double res = *cl->test(in);
+				if (threshold)
+				{
+					if (res > 0.5)
+						glColor4f(0, 1, 0, 0.75);
+					else
+						glColor4f(1, 0, 0, 0.75);
+				}
+				else {
+					glColor3f((1-res)*(1-res), res*res, 0);
+				}
+				glVertex3f(x, y, 0);
+			}
+		}
+		glEnd();
+	}
 }
 
 
@@ -191,20 +199,60 @@ void MyDisplayHandler(unsigned long windowID, tKeyboardModifier mod, char key)
 {
 	switch (key)
 	{
-		case '[': 
-		case ']': break;
-		case '\t':
-			cl.setOutputActivation(kStep);
+		case 'n':
+		{
+			NN = !NN;
+			printf("Neural Network = %s\n", NN?"true":"false");
+			
+			delete cl;
+			delete l;
+			if (!NN)
+			{
+				cl = new LinearRegression(3, 1, 0.01);
+				l = new LinearRegression(2, 1, 0.01);
+			}
+			else {
+//				cl = new class NN(2, 10, 1, 0.01);
+//				l = new class NN(1, 10, 1, 0.01);
+				cl = new LogisticRegression(3, 1, 0.01);
+				l = new LogisticRegression(2, 1, 0.01);
+			}
+			break;
+		}
+		case 'c': CLASSIFIER = !CLASSIFIER;
+			printf("Classifier = %s\n", CLASSIFIER?"true":"false");
+		case '[':
+		case ']': threshold = !threshold; break;
+		case '0':
+			//cl.setOutputActivation(kStep);
+			for (int z = 0; z < 1000; z++)
+			{
+				for (unsigned int x = 0; x < points.size(); x++)
+				{
+					int which = random()%points.size();
+					std::vector<double> in(3);
+					std::vector<double> out(1);
+					in[0] = points[which].loc.x;
+					in[1] = points[which].loc.y;
+					in[2] = in[0]*in[1];//sqrt(in[0]*in[0]+in[1]*in[1]); //
+					out[0] = points[which].val?1:0;
+					//out[0] = points[x].val?1:-1;
+					cl->train(in, out);
+				}
+			}
+		case 't':
+			//cl->setOutputActivation(kStep);
 			for (unsigned int x = 0; x < points.size(); x++)
 			{
 				int which = random()%points.size();
-				std::vector<double> in(2);
+				std::vector<double> in(3);
 				std::vector<double> out(1);
 				in[0] = points[which].loc.x;
 				in[1] = points[which].loc.y;
+				in[2] = in[0]*in[1];//sqrt(in[0]*in[0]+in[1]*in[1]);
 				out[0] = points[which].val?1:0;
 				//out[0] = points[x].val?1:-1;
-				cl.train(in, out);
+				cl->train(in, out);
 			}
 			
 			break;
@@ -213,15 +261,16 @@ void MyDisplayHandler(unsigned long windowID, tKeyboardModifier mod, char key)
 			break;
 		case 'o':
 		{
-			for (int z = 0; z < 1000; z++)
+			for (int z = 0; z < 100; z++)
 			for (unsigned int x = 0; x < points.size(); x++)
 			{
-				std::vector<double> in(1);
+				std::vector<double> in(2);
 				std::vector<double> out(1);
 				in[0] = points[x].loc.x;
-				out[0] = points[x].loc.y/5.0+0.5;
+				in[1] = in[0]*in[0];
+				out[0] = points[x].loc.y/scale+0.5;
 				//out[0] = points[x].val?1:-1;
-				l.train(in, out);
+				l->train(in, out);
 			}
 		}
 			break;
