cf7140a7f1c8a3ea52be32cd5c9963e4821bc38f
Nathan Sturtevant <nathanst@gmail.com>
2015-08-31 14:17:27 -0600

    PDB refactor branch. Lots of changes in how heuristics work, and smaller changes in IDA* and other algorithms.

---
+++ search/PermutationPDB.h
@@ -0,0 +1,13 @@
+//
+//  PermutationPDB.h
+//  hog2 glut
+//
+//  Created by Nathan Sturtevant on 8/27/15.
+//  Copyright (c) 2015 University of Denver. All rights reserved.
+//
+
+#ifndef hog2_glut_PermutationPDB_h
+#define hog2_glut_PermutationPDB_h
+
+
+#endif
