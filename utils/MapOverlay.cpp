ba8917fe5c684d0b6cc711124abe7fbbc29e5121
Nathan Sturtevant <nathanst@gmail.com>
2016-02-17 02:43:26 -0700

    New map overlay color setting functions

--- utils/MapOverlay.cpp
+++ utils/MapOverlay.cpp
@@ -84,9 +84,13 @@ double MapOverlay::GetOverlayValue(int x, int y)
 	return values[y*m->GetMapWidth()+x];
 }
 
-recColor MapOverlay::GetValueColor(double value)
+recColor MapOverlay::GetValueColor(double value) const
 {
-	return getColor(value, minVal, maxVal, colorMap);
+	int v = (int)value;
+	auto loc = colors.find(v);
+	if (loc == colors.end() || colorMap != -1)
+		return getColor(value, minVal, maxVal, 0);
+	return loc->second;
 }
 
 void MapOverlay::OpenGLDraw() const
@@ -98,7 +102,7 @@ void MapOverlay::OpenGLDraw() const
 	else {
 		displayList = glGenLists(1);
 		glNewList(displayList, GL_COMPILE_AND_EXECUTE);
-				
+		
 		glBegin(GL_QUADS);
 		glNormal3f(0, 0, -1);
 		for (unsigned int t = 0; t < values.size(); t++)
@@ -109,7 +113,7 @@ void MapOverlay::OpenGLDraw() const
 				continue;
 			}
 			else {
-				recColor r = getColor(values[t], minVal, maxVal, colorMap);
+				recColor r = GetValueColor(values[t]);// getColor(values[t], minVal, maxVal, colorMap);
 				glColor3f(r.r, r.g, r.b);
 			}
 			unsigned int last;
@@ -136,9 +140,9 @@ void MapOverlay::OpenGLDraw() const
 			glLineWidth(2.0);
 			glColor3f(0.0, 0.0, 0.0);
 			glBegin(GL_LINES);
-			for (int x = 0; x < m->GetMapWidth()-1; x++)
+			for (int y = 0; y < m->GetMapHeight()-1; y++)
 			{
-				for (int y = 0; y < m->GetMapHeight()-1; y++)
+				for (int x = 0; x < m->GetMapWidth()-1; x++)
 				{
 					int index = x+y*m->GetMapWidth();
 					if (values[index] != values[index+1])
@@ -160,88 +164,92 @@ void MapOverlay::OpenGLDraw() const
 			glEnd();
 		}
 		
-		// black background
-		glBegin(GL_QUADS);
-		glNormal3f(0, 0, -1);
-		GLdouble a, b, c, radius;
-		m->GetOpenGLCoord(0, 0, a, b, c, radius);
-		double wide = radius*2.0*m->GetMapWidth()/100.0;
-		glColor3f(0.2, 0.2, 0.2);
-		glVertex3f(a-wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius);
-		glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius);
-		glVertex3f(a-4*wide, b-wide, c-4*radius);
-		glVertex3f(a-wide, b-wide, c-4*radius);
-		glEnd();
 
-		// white border
-		glBegin(GL_LINE_LOOP);
-		glColor3f(1.0, 1.0, 1.0);
-		glVertex3f(a-wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius-radius/10.0);
-		glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius-radius/10.0);
-		glVertex3f(a-4*wide, b-wide, c-4*radius);
-		glVertex3f(a-wide, b-wide, c-4*radius);
-		glEnd();
-
-		// color bar on the side
-		glBegin(GL_QUAD_STRIP);
-		glNormal3f(0, 0, -1);
-		m->GetOpenGLCoord(0, 0, a, b, c, radius);
-		for (int x = 0; x <= stripResolution; x++)
+		if (0)// drawColorBar
 		{
-			recColor r = getColor(minVal + (maxVal-minVal)*((double)x/stripResolution),
-														minVal, maxVal, colorMap);
-			glColor3f(r.r, r.g, r.b);
-			glVertex3f(a-2*wide, b+2*m->GetMapHeight()*radius*(1.0-(double)x/stripResolution), c-4*radius-radius/8.0);
-			glVertex3f(a-3*wide, b+2*m->GetMapHeight()*radius*(1.0-(double)x/stripResolution), c-4*radius-radius/8.0);
-		}
-		glEnd();
+			// black background
+			glBegin(GL_QUADS);
+			glNormal3f(0, 0, -1);
+			GLdouble a, b, c, radius;
+			m->GetOpenGLCoord(0, 0, a, b, c, radius);
+			double wide = radius*2.0*m->GetMapWidth()/100.0;
+			glColor3f(0.2, 0.2, 0.2);
+			glVertex3f(a-wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius);
+			glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius);
+			glVertex3f(a-4*wide, b-wide, c-4*radius);
+			glVertex3f(a-wide, b-wide, c-4*radius);
+			glEnd();
 
-		if (maxVal-minVal != 0)
-		{
-			double mult = 1.0;
-			double range = 1.0;
-			while (1)
-			{
-				if (!(fless((maxVal-minVal)*mult, 10.0)))
-					mult /= 10;
-				else if (fless((maxVal-minVal)*mult, 1.0))
-					mult *= 10;
-				else {
-					if ((maxVal-minVal)*mult >= 5)
-						range = 1.0;
-					else if ((maxVal-minVal)*mult >= 3)
-						range = 0.5;
-					else
-						range = 0.1;
-					break;
-				}
-			}
-			glBegin(GL_LINES);
+			// white border
+			glBegin(GL_LINE_LOOP);
 			glColor3f(1.0, 1.0, 1.0);
-			//double top = (double)((int)(maxVal*mult))/mult;
-			double bottom = (double)((int)(minVal*mult))/mult;
-//			printf("min/max %f, %f, stepped min/max %f, %f\n",
-//						 minVal, maxVal, bottom, top);
-//			printf("Range: %f, mult %f\n", range, mult);
-			for (double x = bottom; !fgreater(x, maxVal); x+=range/mult)
+			glVertex3f(a-wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius-radius/10.0);
+			glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius+wide, c-4*radius-radius/10.0);
+			glVertex3f(a-4*wide, b-wide, c-4*radius);
+			glVertex3f(a-wide, b-wide, c-4*radius);
+			glEnd();
+
+			// color bar on the side
+			glBegin(GL_QUAD_STRIP);
+			glNormal3f(0, 0, -1);
+			m->GetOpenGLCoord(0, 0, a, b, c, radius);
+			for (int x = 0; x <= stripResolution; x++)
 			{
-				if (fequal(x, 0))
-					glColor3f(1.0, 0, 0);
-				glVertex3f(a-1*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
-				glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
-				if (fequal(x, 0))
-					glColor3f(1.0, 1.0, 1.0);
-			}
-			for (double x = bottom-range/mult; !fless(x, minVal); x-=range/mult)
-			{
-				if (fequal(x, 0))
-					glColor3f(1.0, 0, 0);
-				glVertex3f(a-1*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
-				glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
-				if (fequal(x, 0))
-					glColor3f(1.0, 1.0, 1.0);
+				recColor r = getColor(minVal + (maxVal-minVal)*((double)x/stripResolution),
+									  minVal, maxVal, colorMap);
+				glColor3f(r.r, r.g, r.b);
+				glVertex3f(a-2*wide, b+2*m->GetMapHeight()*radius*(1.0-(double)x/stripResolution), c-4*radius-radius/8.0);
+				glVertex3f(a-3*wide, b+2*m->GetMapHeight()*radius*(1.0-(double)x/stripResolution), c-4*radius-radius/8.0);
 			}
 			glEnd();
+			
+			if (maxVal-minVal != 0)
+			{
+				double mult = 1.0;
+				double range = 1.0;
+				while (1)
+				{
+					if (!(fless((maxVal-minVal)*mult, 10.0)))
+						mult /= 10;
+					else if (fless((maxVal-minVal)*mult, 1.0))
+						mult *= 10;
+					else {
+						if ((maxVal-minVal)*mult >= 5)
+							range = 1.0;
+						else if ((maxVal-minVal)*mult >= 3)
+							range = 0.5;
+						else
+							range = 0.1;
+						break;
+					}
+				}
+				glBegin(GL_LINES);
+				glColor3f(1.0, 1.0, 1.0);
+				//double top = (double)((int)(maxVal*mult))/mult;
+				double bottom = (double)((int)(minVal*mult))/mult;
+				//			printf("min/max %f, %f, stepped min/max %f, %f\n",
+				//						 minVal, maxVal, bottom, top);
+				//			printf("Range: %f, mult %f\n", range, mult);
+				for (double x = bottom; !fgreater(x, maxVal); x+=range/mult)
+				{
+					if (fequal(x, 0))
+						glColor3f(1.0, 0, 0);
+					glVertex3f(a-1*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
+					glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
+					if (fequal(x, 0))
+						glColor3f(1.0, 1.0, 1.0);
+				}
+				for (double x = bottom-range/mult; !fless(x, minVal); x-=range/mult)
+				{
+					if (fequal(x, 0))
+						glColor3f(1.0, 0, 0);
+					glVertex3f(a-1*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
+					glVertex3f(a-4*wide, b+2*m->GetMapHeight()*radius*(1.0-(x-minVal)/(maxVal-minVal)), c-4*radius-radius/9.0);
+					if (fequal(x, 0))
+						glColor3f(1.0, 1.0, 1.0);
+				}
+				glEnd();
+			}
 		}
 		
 		glEndList();
--- utils/MapOverlay.h
+++ utils/MapOverlay.h
@@ -11,6 +11,7 @@
 #include "Map.h"
 #include <float.h>
 #include <string>
+#include <unordered_map>
 
 #ifndef MAPOVERLAY_H
 #define MAPOVERLAY_H
@@ -26,9 +27,10 @@ public:
 	void OpenGLDraw() const;
 	std::string SVGDraw() const;
 	void SetTransparentValue(double v) { ignoreVal = v; }
-	recColor GetValueColor(double value);
+	recColor GetValueColor(double value) const;
 	void SetColorMap(int val) { colorMap = val; }
 	int GetColorMap() { return colorMap; }
+	void SetColor(int value, recColor c) { colors[value] = c; }
 	void IncreaseColorMap() { colorMap++; }
 	void DecreaseColorMap() { colorMap--; }
 	double GetMaxValue() { return maxVal; }
@@ -37,6 +39,7 @@ private:
 	void resetValues();
 	Map *m;
 	std::vector<double> values;
+	std::unordered_map<int, recColor> colors;
 	double maxVal, minVal;
 	double ignoreVal;
 	int colorMap;
