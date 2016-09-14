071d87901c8c58ad9a42e5bdc6856e60b2040587
Nathan Sturtevant <nathanst@gmail.com>
2016-09-14 11:47:01 -0600

    Simple reactive agent demonstrating how an agent can move with limited percepts

---
+++ demos/simple-agent/Driver.cpp
@@ -0,0 +1,271 @@
+/*
+ * $Id: sample.cpp,v 1.23 2006/11/01 23:33:56 nathanst Exp $
+ *
+ *  sample.cpp
+ *  hog
+ *
+ *  Created by Nathan Sturtevant on 5/31/05.
+ *  Copyright 2005 Nathan Sturtevant, University of Alberta. All rights reserved.
+ *
+ * This file is part of HOG.
+ *
+ * HOG is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * HOG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with HOG; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "Common.h"
+#include "Driver.h"
+#include "Map2DEnvironment.h"
+#include "CanonicalGrid.h"
+#include "TemplateAStar.h"
+#include "TextOverlay.h"
+#include "OptimisticSearch.h"
+#include "MapGenerators.h"
+#include "MapOverlay.h"
+#include <string>
+#include "RHRUnit.h"
+
+enum mode {
+	kFindPathWA15 = 0,
+	kFindPathWA30 = 1,
+	kFindPathOptimistic = 2
+};
+
+UnitMapSimulation *u;
+MapEnvironment *me = 0;
+TemplateAStar<xyLoc, tDirection, MapEnvironment> astar;
+
+bool running = false;
+
+void StartSearch();
+Map *ReduceMap(Map *inputMap);
+
+int main(int argc, char* argv[])
+{
+	InstallHandlers();
+	RunHOGGUI(argc, argv, 1200, 1200);
+}
+
+/**
+ * Allows you to install any keyboard handlers needed for program interaction.
+ */
+void InstallHandlers()
+{
+	InstallKeyboardHandler(MyDisplayHandler, "Record", "Record a movie", kAnyModifier, 'r');
+	InstallKeyboardHandler(MyDisplayHandler, "Toggle Abstraction", "Toggle display of the ith level of the abstraction", kAnyModifier, '0', '9');
+	InstallKeyboardHandler(MyDisplayHandler, "Cycle Abs. Display", "Cycle which group abstraction is drawn", kAnyModifier, '\t');
+	InstallKeyboardHandler(MyDisplayHandler, "Pause Simulation", "Pause simulation execution.", kNoModifier, 'p');
+	InstallKeyboardHandler(MyDisplayHandler, "Step Simulation", "If the simulation is paused, step forward .1 sec.", kAnyModifier, 'o');
+	InstallKeyboardHandler(MyDisplayHandler, "Step History", "If the simulation is paused, step forward .1 sec in history", kAnyModifier, '}');
+	InstallKeyboardHandler(MyDisplayHandler, "Step History", "If the simulation is paused, step back .1 sec in history", kAnyModifier, '{');
+	InstallKeyboardHandler(MyDisplayHandler, "Step Abs Type", "Increase abstraction type", kAnyModifier, ']');
+	InstallKeyboardHandler(MyDisplayHandler, "Step Abs Type", "Decrease abstraction type", kAnyModifier, '[');
+	InstallKeyboardHandler(MyDisplayHandler, "Clear", "Clear graph", kAnyModifier, '|');
+	InstallKeyboardHandler(MyDisplayHandler, "Help", "Draw help", kAnyModifier, '?');
+	InstallKeyboardHandler(MyDisplayHandler, "Weight", "Toggle Dijkstra & A*", kAnyModifier, 'w');
+	InstallKeyboardHandler(MyDisplayHandler, "Save", "Save current graph", kAnyModifier, 's');
+	InstallKeyboardHandler(MyDisplayHandler, "Load", "Load last saved graph", kAnyModifier, 'l');
+
+	//InstallCommandLineHandler(MyCLHandler, "-map", "-map filename", "Selects the default map to be loaded.");
+	
+	InstallWindowHandler(MyWindowHandler);
+
+	InstallMouseClickHandler(MyClickHandler);
+}
+
+void MyWindowHandler(unsigned long windowID, tWindowEventType eType)
+{
+	if (eType == kWindowDestroyed)
+	{
+		printf("Window %ld destroyed\n", windowID);
+		RemoveFrameHandler(MyFrameHandler, windowID, 0);
+	}
+	else if (eType == kWindowCreated)
+	{
+		printf("Window %ld created\n", windowID);
+		//glClearColor(0.99, 0.99, 0.99, 1.0);
+		InstallFrameHandler(MyFrameHandler, windowID, 0);
+		SetNumPorts(windowID, 1);
+		
+		//Map *map = new Map(300, 300);
+		//MakeRandomMap(map, 40);
+		//MakeMaze(map, 25);
+		//BuildRandomRoomMap(map, 25, 60);
+		
+		//Map *map = new Map("/Users/nathanst/hog2/maps/bgmaps/AR0011SR.map");
+		Map *map = new Map("/Users/nathanst/hog2/maps/bgmaps/AR0012SR.map");
+		//Map *map = new Map("/Users/nathanst/hog2/maps/dao/lak303d.map");
+		//Map *map = new Map("/Users/nathanst/hog2/maps/da2/ht_chantry.map");
+		//Map *map = new Map("/Users/nathanst/hog2/maps/dao/den201d.map");
+
+//		Map *m2 = ReduceMap(map);
+//		delete map;
+//		map = m2;
+
+		map->SetTileSet(kWinter);
+		me = new MapEnvironment(map);
+		u = new UnitMapSimulation(me);
+		u->SetStepType(kUniTime);
+//		start.x = 0xFFFF;
+//		start.y = 0xFFFF;
+	}
+}
+
+int frameCnt = 0;
+
+void MyFrameHandler(unsigned long windowID, unsigned int viewport, void *)
+{
+	//me->OpenGLDraw();
+	u->StepTime(1.0);
+	u->OpenGLDraw();
+}
+
+int MyCLHandler(char *argument[], int maxNumArgs)
+{
+	if (maxNumArgs <= 1)
+		return 0;
+	strncpy(gDefaultMap, argument[1], 1024);
+	return 2;
+}
+
+void MyDisplayHandler(unsigned long windowID, tKeyboardModifier mod, char key)
+{
+	switch (key)
+	{
+		case '{':
+		{
+			break;
+		}
+		case '}':
+		{
+			break;
+		}
+		case ']':
+		{
+		}
+			break;
+		case '[':
+		{
+		}
+			break;
+		case '|':
+		{
+			u->ClearAllUnits();
+		}
+			break;
+		case 'w':
+			break;
+		case 'r':
+			break;
+		case '0':
+//		case '1': edgeCost = 1.0; te.AddLine("Adding edges; New edges cost 1"); m = kAddEdges; break;
+//		case '2': edgeCost = 2.0; te.AddLine("Adding edges; New edges cost 2"); m = kAddEdges; break;
+//		case '3': edgeCost = 3.0; te.AddLine("Adding edges; New edges cost 3"); m = kAddEdges; break;
+//		case '4': edgeCost = 4.0; te.AddLine("Adding edges; New edges cost 4"); m = kAddEdges; break;
+//		case '5': edgeCost = 5.0; te.AddLine("Adding edges; New edges cost 5"); m = kAddEdges; break;
+//		case '6': edgeCost = 6.0; te.AddLine("Adding edges; New edges cost 6"); m = kAddEdges; break;
+//		case '7': edgeCost = 7.0; te.AddLine("Adding edges; New edges cost 7"); m = kAddEdges; break;
+//		case '8': edgeCost = 8.0; te.AddLine("Adding edges; New edges cost 8"); m = kAddEdges; break;
+//		case '9': edgeCost = 9.0; te.AddLine("Adding edges; New edges cost 9"); m = kAddEdges; break;
+		case '\t':
+			
+			if (mod != kShiftDown)
+				SetActivePort(windowID, (GetActivePort(windowID)+1)%GetNumPorts(windowID));
+			else
+			{
+				SetNumPorts(windowID, 1+(GetNumPorts(windowID)%MAXPORTS));
+			}
+			break;
+		case 'p':
+			running = !running;
+			break;
+		case 'o':
+		{
+			if (running)
+			{
+			}
+		}
+			break;
+		case '?':
+		{
+		}
+			break;
+		case 's':
+			break;
+		case 'l':
+			break;
+		default:
+			break;
+	}
+	
+}
+
+bool MyClickHandler(unsigned long , int windowX, int windowY, point3d loc, tButtonType button, tMouseEventType mType)
+{
+	if (mType == kMouseDown)
+	{
+		switch (button)
+		{
+			case kRightButton: printf("Right button\n"); break;
+			case kLeftButton: printf("Left button\n"); break;
+			case kMiddleButton: printf("Middle button\n"); break;
+		}
+	}
+	if (button != kLeftButton)
+		return false;
+	switch (mType)
+	{
+		case kMouseDown:
+		{
+			int x, y;
+			me->GetMap()->GetPointFromCoordinate(loc, x, y);
+			if (me->GetMap()->GetTerrainType(x, y) == kGround)
+			{
+				u->AddUnit(new RHRUnit(x, y));
+			}
+			return true;
+		}
+		case kMouseDrag:
+		{
+//			int x, y;
+//			me->GetMap()->GetPointFromCoordinate(loc, x, y);
+//			if (me->GetMap()->GetTerrainType(x, y) == kGround)
+//			{
+//				goal.x = x;
+//				goal.y = y;
+//			}
+			break;
+		}
+		case kMouseUp:
+		{
+//			int x, y;
+//			me->GetMap()->GetPointFromCoordinate(loc, x, y);
+//			if (me->GetMap()->GetTerrainType(x, y) == kGround)
+//			{
+//				goal.x = x;
+//				goal.y = y;
+//				printf("UnHit (%d, %d)\n", x, y);
+//			}
+//
+//			StartSearch();
+			return true;
+		}
+	}
+	return false;
+}
+
+void StartSearch()
+{
+}
