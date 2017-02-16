bf35c7a437b4c1098b72e9c6e8dea010292171de
Nathan Sturtevant <nathanst@gmail.com>
2017-02-16 12:15:13 -0700

    Fixed using incorrect "size" function.

--- apps/bidirectional/BSStar.h
+++ apps/bidirectional/BSStar.h
@@ -175,14 +175,14 @@ void BSStar<state, action, environment, priorityQueue>::Expand(priorityQueue &cu
 {
 	uint64_t nextID;
 	
-	while (current.size() > 0) {
+	while (current.OpenSize() > 0) {
 		nextID = current.Close();
 		uint64_t reverseLoc;
 		auto loc = opposite.Lookup(env->GetStateHash(current.Lookup(nextID).data), reverseLoc);
 		if (loc != kClosedList)
 			break;
 	}
-	if (current.size() == 0)
+	if (current.OpenSize() == 0)
 		return;
 	// 1. if current closed in opposite direction
 	// 1a. Remove descendents of current in open
