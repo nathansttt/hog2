3b5477068c57053c7bbce64101a901772fa1a402
Nathan Sturtevant <nathanst@gmail.com>
2017-02-17 11:57:10 -0700

    File renaming & new rubik's cube experiments (incomplete)

---
+++ apps/bidirectional/BidirRubik.cpp
@@ -0,0 +1,9 @@
+//
+//  BidirRubik.cpp
+//  hog2 glut
+//
+//  Created by Nathan Sturtevant on 2/15/17.
+//  Copyright © 2017 University of Denver. All rights reserved.
+//
+
+#include "BidirRubik.hpp"
---
+++ apps/bidirectional/BidirRubik.h
@@ -0,0 +1,14 @@
+//
+//  BidirRubik.hpp
+//  hog2 glut
+//
+//  Created by Nathan Sturtevant on 2/15/17.
+//  Copyright © 2017 University of Denver. All rights reserved.
+//
+
+#ifndef BidirRubik_hpp
+#define BidirRubik_hpp
+
+#include <stdio.h>
+
+#endif /* BidirRubik_hpp */
