f1a078fc2d9039a64f6b628966d973f778f5bda6
Nathan Sturtevant <nathanst@gmail.com>
2019-02-21 11:40:49 -0700

    Updated makefiles with new files

---
+++ envutil/GraphInconsistencyInstances.cpp
@@ -0,0 +1,9 @@
+//
+//  GraphInconsistencyInstances.cpp
+//  Inconsistency
+//
+//  Created by Nathan Sturtevant on 2/19/19.
+//  Copyright © 2019 NS Software. All rights reserved.
+//
+
+#include "GraphInconsistencyInstances.hpp"
---
+++ envutil/GraphInconsistencyInstances.h
@@ -0,0 +1,14 @@
+//
+//  GraphInconsistencyInstances.hpp
+//  Inconsistency
+//
+//  Created by Nathan Sturtevant on 2/19/19.
+//  Copyright © 2019 NS Software. All rights reserved.
+//
+
+#ifndef GraphInconsistencyInstances_hpp
+#define GraphInconsistencyInstances_hpp
+
+#include <stdio.h>
+
+#endif /* GraphInconsistencyInstances_hpp */
