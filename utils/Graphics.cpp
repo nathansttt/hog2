56f91b2666df82d02816862d9afd30ba1884f9ff
Nathan Sturtevant <nathanst@gmail.com>
2020-05-27 09:52:35 -0600

    Added support for text baselines.

--- utils/Graphics.cpp
+++ utils/Graphics.cpp
@@ -9,211 +9,221 @@
 #include "Graphics.h"
 
 namespace Graphics {
-	bool PointInRect(const point3d &p, const rect &r)
-	{
-		return p.y >= r.top && p.x >= r.left && p.y <= r.bottom && p.x <= r.right;
-	}
+bool PointInRect(const point3d &p, const rect &r)
+{
+	return p.y >= r.top && p.x >= r.left && p.y <= r.bottom && p.x <= r.right;
+}
 
-	bool PointInRect(const point &p, const rect &r)
-	{
-		return p.y >= r.top && p.x >= r.left && p.y <= r.bottom && p.x <= r.right;
-	}
+bool PointInRect(const point &p, const rect &r)
+{
+	return p.y >= r.top && p.x >= r.left && p.y <= r.bottom && p.x <= r.right;
+}
 
-	Display::Display()
-	{
-		viewport = 0;
-		numViewports = 1;
-		backgroundFrame = foregroundFrame = 0;
-		drawingBackground = false;	}
+Display::Display()
+{
+	viewport = 0;
+	numViewports = 1;
+	backgroundFrame = foregroundFrame = 0;
+	drawingBackground = false;	}
 
-	void Display::StartFrame()
+void Display::StartFrame()
+{
+	drawCommands.clear();
+	text.clear();
+	lineSegments.clear();
+	foregroundFrame++;
+}
+
+void Display::EndFrame()
+{
+}
+
+void Display::StartBackground()
+{
+	if (backgroundFrame != foregroundFrame)
 	{
-		drawCommands.clear();
-		text.clear();
-		lineSegments.clear();
-		foregroundFrame++;
+		backgroundDrawCommands.clear();
+		backgroundText.clear();
+		backgroundLineSegments.clear();
 	}
+	backgroundFrame = foregroundFrame;
+	drawingBackground = true;
+}
+
+void Display::EndBackground()
+{
+	drawingBackground = false;
+}
+
+bool Display::BackgroundNeedsRedraw() const
+{
+	return backgroundFrame == foregroundFrame;
+}
+
+void Display::SetNumViewports(uint8_t v)
+{
+	numViewports = v;
+}
+
+void Display::SetViewport(uint8_t v)
+{
+	viewport = v;
+}
+
+void Display::FrameRect(rect r, rgbColor c, float lineWidth)
+{
+	drawInfo i = {r, c, lineWidth};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFrameRectangle, viewport});
+	else
+		drawCommands.push_back({i, kFrameRectangle, viewport});
+}
+
+void Display::FrameSquare(point p, float radius, rgbColor c, float lineWidth)
+{
+	drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, lineWidth};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFrameRectangle, viewport});
+	else
+		drawCommands.push_back({i, kFrameRectangle, viewport});
 	
-	void Display::EndFrame()
-	{
-	}
+}
 
-	void Display::StartBackground()
-	{
-		if (backgroundFrame != foregroundFrame)
-		{
-			backgroundDrawCommands.clear();
-			backgroundText.clear();
-			backgroundLineSegments.clear();
-		}
-		backgroundFrame = foregroundFrame;
-		drawingBackground = true;
-	}
-	
-	void Display::EndBackground()
-	{
-		drawingBackground = false;
-	}
+void Display::FillSquare(point p, float radius, rgbColor c)
+{
+	drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, 0};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFillRectangle, viewport});
+	else
+		drawCommands.push_back({i, kFillRectangle, viewport});
+}
 
-	bool Display::BackgroundNeedsRedraw() const
-	{
-		return backgroundFrame == foregroundFrame;
-	}
-	
-	void Display::SetNumViewports(uint8_t v)
-	{
-		numViewports = v;
-	}
+void Display::FillRect(rect r, rgbColor c)
+{
+	drawInfo i = {r, c, 0};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFillRectangle, viewport});
+	else
+		drawCommands.push_back({i, kFillRectangle, viewport});
+}
 
-	void Display::SetViewport(uint8_t v)
-	{
-		viewport = v;
-	}
+void Display::FrameCircle(rect r, rgbColor c, float lineWidth)
+{
+	drawInfo i = {r, c, lineWidth};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFrameOval, viewport});
+	else
+		drawCommands.push_back({i, kFrameOval, viewport});
+}
 
-	void Display::FrameRect(rect r, rgbColor c, float lineWidth)
-	{
-		drawInfo i = {r, c, lineWidth};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFrameRectangle, viewport});
-		else
-			drawCommands.push_back({i, kFrameRectangle, viewport});
-	}
+void Display::FrameCircle(point p, float radius, rgbColor c, float lineWidth)
+{
+	drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, lineWidth};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFrameOval, viewport});
+	else
+		drawCommands.push_back({i, kFrameOval, viewport});
+}
 
-	void Display::FrameSquare(point p, float radius, rgbColor c, float lineWidth)
-	{
-		drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, lineWidth};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFrameRectangle, viewport});
-		else
-			drawCommands.push_back({i, kFrameRectangle, viewport});
+void Display::FillCircle(rect r, rgbColor c)
+{
+	drawInfo i = {r, c, 0};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFillOval, viewport});
+	else
+		drawCommands.push_back({i, kFillOval, viewport});
+}
 
-	}
+void Display::FillCircle(point p, float radius, rgbColor c)
+{
+	drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, 0};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFillOval, viewport});
+	else
+		drawCommands.push_back({i, kFillOval, viewport});
+}
 
-	void Display::FillSquare(point p, float radius, rgbColor c)
-	{
-		drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, 0};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFillRectangle, viewport});
-		else
-			drawCommands.push_back({i, kFillRectangle, viewport});
-	}
-	
-	void Display::FillRect(rect r, rgbColor c)
-	{
-		drawInfo i = {r, c, 0};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFillRectangle, viewport});
-		else
-			drawCommands.push_back({i, kFillRectangle, viewport});
-	}
-	
-	void Display::FrameCircle(rect r, rgbColor c, float lineWidth)
-	{
-		drawInfo i = {r, c, lineWidth};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFrameOval, viewport});
-		else
-			drawCommands.push_back({i, kFrameOval, viewport});
-	}
-	
-	void Display::FrameCircle(point p, float radius, rgbColor c, float lineWidth)
-	{
-		drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, lineWidth};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFrameOval, viewport});
-		else
-			drawCommands.push_back({i, kFrameOval, viewport});
-	}
+void Display::FillNGon(point p, float radius, int sides, float rotation, rgbColor c)
+{
+	shapeInfo i = {p, c, radius, sides, rotation};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFillNGon, viewport});
+	else
+		drawCommands.push_back({i, kFillNGon, viewport});
+}
 
-	void Display::FillCircle(rect r, rgbColor c)
-	{
-		drawInfo i = {r, c, 0};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFillOval, viewport});
-		else
-			drawCommands.push_back({i, kFillOval, viewport});
-	}
+void Display::FrameNGon(point p, float radius, int sides, float rotation, rgbColor c)
+{
+	shapeInfo i = {p, c, radius, sides, rotation};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, kFrameNGon, viewport});
+	else
+		drawCommands.push_back({i, kFrameNGon, viewport});
+}
 
-	void Display::FillCircle(point p, float radius, rgbColor c)
-	{
-		drawInfo i = {{p.x-radius, p.y-radius, p.x+radius, p.y+radius}, c, 0};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFillOval, viewport});
-		else
-			drawCommands.push_back({i, kFillOval, viewport});
-	}
 
-	void Display::FillNGon(point p, float radius, int sides, float rotation, rgbColor c)
-	{
-		shapeInfo i = {p, c, radius, sides, rotation};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFillNGon, viewport});
-		else
-			drawCommands.push_back({i, kFillNGon, viewport});
-	}
-	
-	void Display::FrameNGon(point p, float radius, int sides, float rotation, rgbColor c)
-	{
-		shapeInfo i = {p, c, radius, sides, rotation};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, kFrameNGon, viewport});
-		else
-			drawCommands.push_back({i, kFrameNGon, viewport});
-	}
+void Display::DrawLine(point start, point end, float lineWidth, rgbColor c)
+{
+	lineInfo i = {start, end, c, lineWidth, false};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, viewport});
+	else
+		drawCommands.push_back({i, viewport});
+}
 
-	
-	void Display::DrawLine(point start, point end, float lineWidth, rgbColor c)
-	{
-		lineInfo i = {start, end, c, lineWidth, false};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, viewport});
-		else
-			drawCommands.push_back({i, viewport});
-	}
+void Display::DrawArrow(point start, point end, float lineWidth, rgbColor c)
+{
+	lineInfo i = {start, end, c, lineWidth, true};
+	if (drawingBackground)
+		backgroundDrawCommands.push_back({i, viewport});
+	else
+		drawCommands.push_back({i, viewport});
+}
 
-	void Display::DrawArrow(point start, point end, float lineWidth, rgbColor c)
-	{
-		lineInfo i = {start, end, c, lineWidth, true};
-		if (drawingBackground)
-			backgroundDrawCommands.push_back({i, viewport});
-		else
-			drawCommands.push_back({i, viewport});
-	}
+void Display::DrawText(const char *textString, point location, rgbColor c, float height,
+					   textAlign align, textBaseline base, const char *typeface)
+{
+	textInfo i = {std::string(textString), location, c, height, std::string((typeface==0)?"Helvetica":typeface), align, base, viewport};
+	if (drawingBackground)
+		backgroundText.push_back(i);
+	else
+		text.push_back(i);
+}
 
-	void Display::DrawText(const char *textString, point location, rgbColor c, float height, textAlign align, const char *typeface)
-	{
-		textInfo i = {std::string(textString), location, c, height, std::string((typeface==0)?"Helvetica":typeface), align, viewport};
-		if (drawingBackground)
-			backgroundText.push_back(i);
-		else
-			text.push_back(i);
-	}
+void Display::DrawText(const char *textString, point location, rgbColor c, float height, textAlign align, const char *typeface)
+{
+	textInfo i = {std::string(textString), location, c, height, std::string((typeface==0)?"Helvetica":typeface), align, textBaselineBottom, viewport};
+	if (drawingBackground)
+		backgroundText.push_back(i);
+	else
+		text.push_back(i);
+}
 
-	void Display::DrawText(const char *textString, point location, rgbColor c, float height, const char *typeface)
-	{
-		textInfo i = {std::string(textString), location, c, height, std::string((typeface==0)?"Helvetica":typeface), textAlignLeft, viewport};
-		if (drawingBackground)
-			backgroundText.push_back(i);
-		else
-			text.push_back(i);
-	}
+void Display::DrawText(const char *textString, point location, rgbColor c, float height, const char *typeface)
+{
+	textInfo i = {std::string(textString), location, c, height, std::string((typeface==0)?"Helvetica":typeface), textAlignLeft, textBaselineBottom, viewport};
+	if (drawingBackground)
+		backgroundText.push_back(i);
+	else
+		text.push_back(i);
+}
 
-	void Display::DrawLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c)
-	{
-		segments s = {c, lineWidth, points, false, viewport};
-		if (drawingBackground)
-			backgroundLineSegments.push_back(s);
-		else
-			lineSegments.push_back(s);
-	}
+void Display::DrawLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c)
+{
+	segments s = {c, lineWidth, points, false, viewport};
+	if (drawingBackground)
+		backgroundLineSegments.push_back(s);
+	else
+		lineSegments.push_back(s);
+}
 
-	void Display::FillLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c)
-	{
-		segments s = {c, lineWidth, points, true, viewport};
-		if (drawingBackground)
-			backgroundLineSegments.push_back(s);
-		else
-			lineSegments.push_back(s);
-	}
+void Display::FillLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c)
+{
+	segments s = {c, lineWidth, points, true, viewport};
+	if (drawingBackground)
+		backgroundLineSegments.push_back(s);
+	else
+		lineSegments.push_back(s);
+}
 
 }
--- utils/Graphics.h
+++ utils/Graphics.h
@@ -15,207 +15,216 @@
 #include "FPUtil.h"
 
 namespace Graphics {
-	class point;
+class point;
 
-	enum textAlign {
-		textAlignCenter,
-		textAlignLeft
-	};
+enum textAlign {
+	textAlignCenter,
+	textAlignLeft,
+	textAlignRight
+};
+
+enum textBaseline {
+	textBaselineTop,
+	textBaselineMiddle,
+	textBaselineBottom
+};
 
 
-	struct point {
-		point(point3d p) :x(p.x), y(p.y), z(p.z) {}
-		point(float x = 0, float y = 0, float z = 0)
-		:x(x), y(y), z(z) {}
-		float x, y, z;
-
-		bool operator==(const point &p) const
-		{ return fequal(p.x, x) && fequal(p.y, y) && fequal(p.z, z); }
-
-		point &operator*=(float i)
-		{ x*= i; y*=i; z*=i; return *this; }
-		point &operator+=(const point &i)
-		{ x+=i.x; y+=i.y; z+=i.z; return *this; }
-		point &operator-=(const point &i)
-		{ x-=i.x; y-=i.y; z-=i.z; return *this; }
-		point operator*(float i)
-		{ point p = *this; p*=i; return p; }
-		point operator+(const point &i)
-		{ point p = *this; p+=i; return p; }
-		point operator-(const point &i)
-		{ point p = *this; p-=i; return p; }
-		float length() const
-		{ return sqrtf(x * x + y * y + z * z); }
-		void normalise()
-		{
-			float length = this->length();
-			if (length != 0)
-			{
-				x /= length; y /= length; z /= length;
-			}
-			else {
-				x = 0; y = 0; z = 0;
-			}
-		}
-		point operator*(const point &val) const
-		{
-			point result;
-			result.x = this->y*val.z - this->z*val.y;
-			result.y = this->z*val.x - this->x*val.z;
-			result.z = this->x*val.y - this->y*val.x;
-			result.normalise();
-			return result;
-		}
-	};
+struct point {
+	point(point3d p) :x(p.x), y(p.y), z(p.z) {}
+	point(float x = 0, float y = 0, float z = 0)
+	:x(x), y(y), z(z) {}
+	float x, y, z;
 	
-	struct rect {
-		rect() {}
-		rect(point center, float rad) : left(center.x-rad), top(center.y-rad), right(center.x+rad), bottom(center.y+rad) {}
-		rect(float l, float t, float r, float b)
-		:left(l), top(t), right(r), bottom(b) {}
-		float left, top, right, bottom;
-
-		rect &operator|=(const rect &val)
+	bool operator==(const point &p) const
+	{ return fequal(p.x, x) && fequal(p.y, y) && fequal(p.z, z); }
+	
+	point &operator*=(float i)
+	{ x*= i; y*=i; z*=i; return *this; }
+	point &operator+=(const point &i)
+	{ x+=i.x; y+=i.y; z+=i.z; return *this; }
+	point &operator-=(const point &i)
+	{ x-=i.x; y-=i.y; z-=i.z; return *this; }
+	point operator*(float i)
+	{ point p = *this; p*=i; return p; }
+	point operator+(const point &i)
+	{ point p = *this; p+=i; return p; }
+	point operator-(const point &i)
+	{ point p = *this; p-=i; return p; }
+	float length() const
+	{ return sqrtf(x * x + y * y + z * z); }
+	void normalise()
+	{
+		float length = this->length();
+		if (length != 0)
 		{
-			left = std::min(left, val.left);
-			right = std::max(right, val.right);
-			top = std::min(top, val.top);
-			bottom = std::max(bottom, val.bottom);
-			return *this;
+			x /= length; y /= length; z /= length;
 		}
+		else {
+			x = 0; y = 0; z = 0;
+		}
+	}
+	point operator*(const point &val) const
+	{
+		point result;
+		result.x = this->y*val.z - this->z*val.y;
+		result.y = this->z*val.x - this->x*val.z;
+		result.z = this->x*val.y - this->y*val.x;
+		result.normalise();
+		return result;
+	}
+};
+
+struct rect {
+	rect() {}
+	rect(point center, float rad) : left(center.x-rad), top(center.y-rad), right(center.x+rad), bottom(center.y+rad) {}
+	rect(float l, float t, float r, float b)
+	:left(l), top(t), right(r), bottom(b) {}
+	float left, top, right, bottom;
+	
+	rect &operator|=(const rect &val)
+	{
+		left = std::min(left, val.left);
+		right = std::max(right, val.right);
+		top = std::min(top, val.top);
+		bottom = std::max(bottom, val.bottom);
+		return *this;
+	}
+};
+
+inline std::ostream &operator<<(std::ostream &o, const rect&r)
+{ o << r.left << ", " << r.top  << ", " << r.right << ", " << r.bottom; return o; }
+
+inline std::ostream &operator<<(std::ostream &o, const point&r)
+{ o << "(" << r.x << ", " << r.y  << ", " << r.z << ")"; return o; }
+
+bool PointInRect(const point3d &p, const rect &r);
+bool PointInRect(const point &p, const rect &r);
+
+/*
+ * This class represents an abstract display.
+ *
+ * All hog2 classes do their drawing to the display. This is an abstraction for a true
+ * display. Any actual display then must collect the data from this display and show it,
+ * as appropriate.
+ */
+class Display {
+public:
+	Display();
+	void StartFrame();
+	void EndFrame();
+	void StartBackground();
+	void EndBackground();
+	bool BackgroundNeedsRedraw() const;
+	void SetViewport(uint8_t v);
+	void SetNumViewports(uint8_t v);
+	int GetNumViewports() { return numViewports; }
+	void FrameRect(rect r, rgbColor c, float lineWidth);
+	void FillRect(rect r, rgbColor c);
+	void FrameSquare(point p, float radius, rgbColor c, float lineWidth);
+	void FillSquare(point p, float radius, rgbColor c);
+	void FrameCircle(rect r, rgbColor c, float lineWidth); // FIXME: Should be a point and a radius!
+	void FrameCircle(point r, float radius, rgbColor c, float lineWidth); // FIXME: Should be a point and a radius!
+	void FillCircle(rect r, rgbColor c);
+	void FillCircle(point p, float radius, rgbColor c);
+	void FillNGon(point p, float radius, int sides, float rotation, rgbColor c);
+	void FrameNGon(point p, float radius, int sides, float rotation, rgbColor c);
+	
+	void DrawLine(point start, point end, float lineWidth, rgbColor c);
+	void DrawLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c);
+	void FillLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c);
+	void DrawArrow(point start, point end, float lineWidth, rgbColor c);
+	void DrawText(const char *text, point location, rgbColor c, float height, const char *typeface = 0);
+	void DrawText(const char *text, point location, rgbColor c, float height, textAlign align, const char *typeface = 0);
+	void DrawText(const char *text, point location, rgbColor c, float height, textAlign align, textBaseline base, const char *typeface = 0);
+
+	struct drawInfo {
+		rect r;
+		rgbColor c;
+		float width;
 	};
-	
-	inline std::ostream &operator<<(std::ostream &o, const rect&r)
-	{ o << r.left << ", " << r.top  << ", " << r.right << ", " << r.bottom; return o; }
-	
-	inline std::ostream &operator<<(std::ostream &o, const point&r)
-	{ o << "(" << r.x << ", " << r.y  << ", " << r.z << ")"; return o; }
-
-	bool PointInRect(const point3d &p, const rect &r);
-	bool PointInRect(const point &p, const rect &r);
-
-	/*
-	 * This class represents an abstract display.
-	 *
-	 * All hog2 classes do their drawing to the display. This is an abstraction for a true
-	 * display. Any actual display then must collect the data from this display and show it,
-	 * as appropriate.
-	 */
-	class Display {
-	public:
-		Display();
-		void StartFrame();
-		void EndFrame();
-		void StartBackground();
-		void EndBackground();
-		bool BackgroundNeedsRedraw() const;
-		void SetViewport(uint8_t v);
-		void SetNumViewports(uint8_t v);
-		int GetNumViewports() { return numViewports; }
-		void FrameRect(rect r, rgbColor c, float lineWidth);
-		void FillRect(rect r, rgbColor c);
-		void FrameSquare(point p, float radius, rgbColor c, float lineWidth);
-		void FillSquare(point p, float radius, rgbColor c);
-		void FrameCircle(rect r, rgbColor c, float lineWidth); // FIXME: Should be a point and a radius!
-		void FrameCircle(point r, float radius, rgbColor c, float lineWidth); // FIXME: Should be a point and a radius!
-		void FillCircle(rect r, rgbColor c);
-		void FillCircle(point p, float radius, rgbColor c);
-		void FillNGon(point p, float radius, int sides, float rotation, rgbColor c);
-		void FrameNGon(point p, float radius, int sides, float rotation, rgbColor c);
-
-		void DrawLine(point start, point end, float lineWidth, rgbColor c);
-		void DrawLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c);
-		void FillLineSegments(const std::vector<point> &points, float lineWidth, rgbColor c);
-		void DrawArrow(point start, point end, float lineWidth, rgbColor c);
-		void DrawText(const char *text, point location, rgbColor c, float height, const char *typeface = 0);
-		void DrawText(const char *text, point location, rgbColor c, float height, textAlign align, const char *typeface = 0);
-
-		struct drawInfo {
-			rect r;
-			rgbColor c;
-			float width;
-		};
-		struct shapeInfo {
-			point center;
-			rgbColor c;
-			float radius;
-			int segments;
-			float rotate;
-		};
-		struct lineInfo {
-			point start, end;
-			rgbColor c;
-			float width;
-			bool arrow;
-		};
-		struct textInfo {
-			std::string s;
-			point loc;
-			rgbColor c;
-			float size;
-			std::string typeface;
-			textAlign align;
-			uint8_t viewport;
-		};
-		enum tDrawClass
-		{
-			kFillRectangle,
-			kFrameRectangle,
-			kFillOval,
-			kFrameOval,
-			kFillNGon,
-			kFrameNGon,
-			kLine
-		};
-		struct data {
-			data(shapeInfo d, tDrawClass t, uint8_t view)
-			{
-				what = t;
-				polygon = d;
-				viewport = view;
-			}
-			data(drawInfo d, tDrawClass t, uint8_t view)
-			{
-				what = t;
-				shape = d;
-				viewport = view;
-			}
-			data(lineInfo l, uint8_t view)
-			{
-				what = kLine;
-				line = l;
-				viewport = view;
-			}
-			tDrawClass what;
-			union {
-				drawInfo shape;
-				shapeInfo polygon;
-				lineInfo line;
-			};
-			uint8_t viewport;
-		};
-		struct segments {
-			rgbColor c;
-			float size;
-			std::vector<point> points;
-			bool fill;
-			uint8_t viewport;
-		};
-		// These are dynamic items that change from frame to frame
-		std::vector<data> drawCommands;
-		std::vector<textInfo> text;
-		std::vector<segments> lineSegments;
-		// These are static items that don't usually change from frame to frame
-		std::vector<data> backgroundDrawCommands;
-		std::vector<textInfo> backgroundText;
-		std::vector<segments> backgroundLineSegments;
-		uint64_t backgroundFrame;
-		uint64_t foregroundFrame;
-	private:
+	struct shapeInfo {
+		point center;
+		rgbColor c;
+		float radius;
+		int segments;
+		float rotate;
+	};
+	struct lineInfo {
+		point start, end;
+		rgbColor c;
+		float width;
+		bool arrow;
+	};
+	struct textInfo {
+		std::string s;
+		point loc;
+		rgbColor c;
+		float size;
+		std::string typeface;
+		textAlign align;
+		textBaseline base;
 		uint8_t viewport;
-		uint8_t numViewports;
-		bool drawingBackground;
 	};
+	enum tDrawClass
+	{
+		kFillRectangle,
+		kFrameRectangle,
+		kFillOval,
+		kFrameOval,
+		kFillNGon,
+		kFrameNGon,
+		kLine
+	};
+	struct data {
+		data(shapeInfo d, tDrawClass t, uint8_t view)
+		{
+			what = t;
+			polygon = d;
+			viewport = view;
+		}
+		data(drawInfo d, tDrawClass t, uint8_t view)
+		{
+			what = t;
+			shape = d;
+			viewport = view;
+		}
+		data(lineInfo l, uint8_t view)
+		{
+			what = kLine;
+			line = l;
+			viewport = view;
+		}
+		tDrawClass what;
+		union {
+			drawInfo shape;
+			shapeInfo polygon;
+			lineInfo line;
+		};
+		uint8_t viewport;
+	};
+	struct segments {
+		rgbColor c;
+		float size;
+		std::vector<point> points;
+		bool fill;
+		uint8_t viewport;
+	};
+	// These are dynamic items that change from frame to frame
+	std::vector<data> drawCommands;
+	std::vector<textInfo> text;
+	std::vector<segments> lineSegments;
+	// These are static items that don't usually change from frame to frame
+	std::vector<data> backgroundDrawCommands;
+	std::vector<textInfo> backgroundText;
+	std::vector<segments> backgroundLineSegments;
+	uint64_t backgroundFrame;
+	uint64_t foregroundFrame;
+private:
+	uint8_t viewport;
+	uint8_t numViewports;
+	bool drawingBackground;
+};
 
 }
 
