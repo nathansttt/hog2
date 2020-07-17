590ac082c30505efae3e29f16ef15e56cfe07e33
Nathan Sturtevant <nathanst@gmail.com>
2020-07-17 16:47:26 -0600

    Simplified editing code.

--- apps/snakebird/Driver.h
+++ apps/snakebird/Driver.h
@@ -5,7 +5,7 @@
 
 void MyWindowHandler(unsigned long windowID, tWindowEventType eType);
 void MyFrameHandler(unsigned long windowID, unsigned int viewport, void *data);
-void MyDisplayHandler(unsigned long windowID, tKeyboardModifier, char key);
+void GamePlayKeyboardHandler(unsigned long windowID, tKeyboardModifier, char key);
 void EditorKeyBoardHandler(unsigned long windowID, tKeyboardModifier, char key);
 int MyCLHandler(char *argument[], int maxNumArgs);
 bool MyClickHandler(unsigned long windowID, int x, int y, point3d loc, tButtonType, tMouseEventType);
