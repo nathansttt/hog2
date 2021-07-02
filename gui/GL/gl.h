fffb5ec032fcc0b7f1ea63e9775ce3aa24c57c6a
Nathan Sturtevant <nathanst@gmail.com>
2021-07-02 09:21:12 -0600

    Added stub libraries

--- gui/GL/gl.h
+++ gui/GL/gl.h
@@ -674,5 +674,9 @@ void glDrawArrays (GLenum mode, GLint first, GLsizei count);
 void glNormalPointer (GLenum type, GLsizei stride, const GLvoid *pointer);
 void glDrawElements (GLenum mode, GLsizei count, GLenum type, const GLvoid *indices);
 
+void glMultMatrixd(	const GLdouble * m);
+void glMultMatrixf(	const GLfloat * m);
+
+	
 }
 #endif
