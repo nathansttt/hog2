4c30240095a071ce50458fdc916264f162424ad5
Marcus Kim <marcus5@ualberta.ca>
2021-09-06 14:05:51 -0600

    Commit implementation of RC demo, including the projection of the cube to 3D, and animation of cube actions.
    RCState state arrays are present and update with cube actions, though the cube is not drawn using them.
    
    modified:   build/SFML/demos/rc/Makefile.prj.inc
    modified:   demos/rc/Driver.cpp
    modified:   environments/RC.cpp
    modified:   environments/RC.h

--- environments/RC.cpp
+++ environments/RC.cpp
@@ -10,6 +10,683 @@
 #include <algorithm>
 #include <string>
 
+// Helpers (for calculation)
+void EditPointMult(float (&vec)[3][3], Graphics::point & p);
+void MakeMatrixRotX(float (&a)[3][3], float angle);
+void MakeMatrixRotY(float (&a)[3][3], float angle);
+void MakeMatrixRotZ(float (&a)[3][3], float angle);
+float array3 [3] = {0,0,0};
+float rotation [3][3] = { {0,0,0}, {0,0,0}, {0,0,0} };
+std::vector<int> furthestZ(0);
+//std::vector<std::vector<float>> rotation(3,std::vector<float>(3));
+
+/*===============================================================================================================================================================================
+ * 			CUBIE CLASS
+ ===============================================================================================================================================================================*/
+
+void Cubie::Initialize(int ind)
+{
+	index = ind;
+	int xPos = 0, yPos = 0, zPos = 0;
+	//Cube index (out of 26)
+
+	int in = index;
+	if (in >= 13) in++; //Readd middle cubie for init ease
+		//Initialize face colors, determine position based on index
+		for (int i = 0; i < 6; i++)
+		{
+			faceCols[i] = inCol;
+			facesShown[i] = false;
+		}
+	//If its cube #0,1,2,9,10,11,18,19,20, make face 0 white
+	for (int i = 0; i < 3; i++)
+	{
+		if (in >= i*9 && in <= i*9 + 2)
+		{
+			faceCols[0] = faceColors[0];
+			facesShown[0] = true;
+			yPos--;
+		}
+	}
+	//If its cube #0-8, make face 1 blue
+	if (in >= 0 && in <= 8)
+	{
+		faceCols[1] = faceColors[1];
+		facesShown[1] = true;
+		zPos--;
+	}
+	//If its cube # is a multiple of 3 (including 0), make face 2 red
+	if (in % 3 == 0)
+	{
+		faceCols[2] = faceColors[2];
+		facesShown[2] = true;
+		xPos--;
+	}
+	//If its cube #18-26, make face 3 green
+	if (in >= 18 && in <= 26)
+	{
+		faceCols[3] = faceColors[3];
+		facesShown[3] = true;
+		zPos++;
+		//std::cout << in << '\n';
+	}
+	//If its cube # - 2 is multiple of 3, make face 4 orange
+	if ((in-2) % 3 == 0)
+	{
+		faceCols[4] = faceColors[4];
+		facesShown[4] = true;
+		xPos++;
+	}
+	//If its cube #6,7,8,15,16,17,24,25,26, make face 5 yellow
+	for (int i = 0; i < 3; i++)
+	{
+		if (in >= i*9 + 6 && in <= i*9 + 8)
+		{
+			faceCols[5] = faceColors[5];
+			facesShown[5] = true;
+			yPos++;
+		}
+	}
+
+	//Initialize the 8 points
+	for (int i = 0; i < 8; i++)
+	{
+		points.push_back(Graphics::point(0,0,0));
+		points[i].x = -0.5f * sideLen;// + xPos * sideLen;
+		if ((i%4) == 1 || (i%4) == 2)
+			points[i].x += 1.f * sideLen;
+		points[i].y = (-0.5f + (1.f * ((i/2)%2))) * sideLen;// + yPos * sideLen; //y
+		points[i].z = (-0.5f + (1.f * (i/4))) * sideLen;// + zPos * sideLen; //z
+		
+		basePoints.push_back(Graphics::point(points[i].x,points[i].y,points[i].z));
+	}
+	//Initialize the 4 face points for all 6 faces (24)
+	for (int i = 0; i < 6; i++)
+	{
+		facePoints.push_back(std::vector<Graphics::point>());
+		baseFacePoints.push_back(std::vector<Graphics::point>());
+		for (int j = 0; j < 4; j++)
+		{
+			float x, y, z;
+			if (i == 2 || i == 4) x = 1.0f;
+				else x = faceSize;
+			if (i == 0 || i == 5) y = 1.0f;
+				else y = faceSize;
+			if (i == 1 || i == 3) z = 1.0f;
+				else z = faceSize;
+
+			Graphics::point p = points[pointsOnFace[i][j]];
+			x *= p.x;
+			y *= p.y;
+			z *= p.z;
+			facePoints[i].push_back(Graphics::point(x,y,z));
+			baseFacePoints[i].push_back(Graphics::point(x,y,z));
+		}
+	}
+	//Move all points (faces and cube) by coord
+	for (int i = 0; i < 8; i++)
+	{
+		points[i].x += xPos * sideLen;
+		points[i].y += yPos * sideLen;
+		points[i].z += zPos * sideLen;
+		basePoints[i].x += xPos * sideLen;
+		basePoints[i].y += yPos * sideLen;
+		basePoints[i].z += zPos * sideLen;
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			facePoints[i][j].x += xPos * sideLen;
+			facePoints[i][j].y += yPos * sideLen;
+			facePoints[i][j].z += zPos * sideLen;
+			baseFacePoints[i][j].x += xPos * sideLen;
+			baseFacePoints[i][j].y += yPos * sideLen;
+			baseFacePoints[i][j].z += zPos * sideLen;
+		}
+	}
+}
+
+/*
+ * 
+ */
+void Cubie::Draw(Graphics::Display &display)
+{
+	//	Identify which faces will be hidden from sight (Find which points are the furthest back)
+	float highest;
+	furthestZ.clear();
+	furthestZ.push_back(0);
+	highest = points[0].z;
+	for (int i = 1; i < 8; i++)
+	{
+		if (points[i].z > highest) {
+			furthestZ.clear();
+			furthestZ.push_back(i);
+			highest = points[i].z;
+			continue;
+		}
+		if (points[i].z == highest) {
+			furthestZ.push_back(i);
+		}
+	}
+	int facesDrawn = 0;
+	for (int i = 0; i < 6; i++)
+	{
+		if (facesShown[i])
+		{
+			bool drawThis = true;
+
+			for (int pInd : pointsOnFace[i])
+			{
+				for (int check : furthestZ)
+				{
+					if (pInd == check)
+					{
+						//This face shouldn't be shown
+						drawThis = false;
+						break;
+					}
+				}
+			}
+
+			//Draw face if not on the back
+			if (drawThis)
+			{
+				facesDrawn++;
+				DrawFace(display, i);
+			}
+		}
+	}
+}
+
+/* Given points (in a correct order)
+ * Draw a polygon (face of a cubie)
+ */
+void Cubie::DrawFace(Graphics::Display &display, int index) //face Index (0-5)
+{
+	display.FillTriangle( points[pointsOnFace[index][0]], points[pointsOnFace[index][1]],
+			points[pointsOnFace[index][2]], outCol);
+	display.FillTriangle( points[pointsOnFace[index][0]], points[pointsOnFace[index][2]],
+			points[pointsOnFace[index][3]], outCol);
+
+	display.FillTriangle( facePoints[index][0], facePoints[index][1],
+			facePoints[index][2], faceCols[index]);
+	display.FillTriangle( facePoints[index][0], facePoints[index][2],
+			facePoints[index][3], faceCols[index]);
+}
+
+/*
+ * 
+ */
+void Cubie::RotateRelative(float angle [3])
+{
+	for (Graphics::point & point : points) //!!!!
+	{
+		MakeMatrixRotX(rotation, angle[0]);
+		EditPointMult(rotation, point);
+		MakeMatrixRotY(rotation, angle[1]);
+		EditPointMult(rotation, point);
+		MakeMatrixRotZ(rotation, angle[2]);
+		EditPointMult(rotation, point);
+	}
+	for (int i = 0; i < 6; i++) //!!!!
+	{
+		MakeMatrixRotX(rotation, angle[0]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, facePoints[i][j]);
+		MakeMatrixRotY(rotation, angle[1]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, facePoints[i][j]);
+		MakeMatrixRotZ(rotation, angle[2]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, facePoints[i][j]);
+	}
+}
+
+/*
+ * 
+ */
+void Cubie::RotateBase(float angle [3])
+{
+	for (Graphics::point & point : basePoints)
+	{
+		MakeMatrixRotX(rotation, angle[0]);
+		EditPointMult(rotation, point);
+		MakeMatrixRotY(rotation, angle[1]);
+		EditPointMult(rotation, point);
+		MakeMatrixRotZ(rotation, angle[2]);
+		EditPointMult(rotation, point);
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		MakeMatrixRotX(rotation, angle[0]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, baseFacePoints[i][j]);
+		MakeMatrixRotY(rotation, angle[1]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, baseFacePoints[i][j]);
+		MakeMatrixRotZ(rotation, angle[2]);
+		for (int j = 0; j < 4; j++)
+			EditPointMult(rotation, baseFacePoints[i][j]);
+	}
+}
+
+/*
+ * 
+ */
+void Cubie::RotateFacePos(bool clockwise, int axis)
+{
+	int temp;
+	if (clockwise)
+	{
+		temp = faceInPos[faceOrderByAxis[axis][3]];
+		for (int i = 3; i > 0; i--)
+		{
+			faceInPos[faceOrderByAxis[axis][i]] = faceInPos[faceOrderByAxis[axis][i-1]];
+		}
+		faceInPos[faceOrderByAxis[axis][0]] = temp;
+	} else
+	{
+		temp = faceInPos[faceOrderByAxis[axis][0]];
+		for (int i = 0; i < 3; i++)
+		{
+			faceInPos[faceOrderByAxis[axis][i]] = faceInPos[faceOrderByAxis[axis][i+1]];
+		}
+		faceInPos[faceOrderByAxis[axis][3]] = temp;
+	}
+}
+
+/*
+ * 
+ */
+void Cubie::ResetToBase()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		points[i].x = basePoints[i].x;
+		points[i].y = basePoints[i].y;
+		points[i].z = basePoints[i].z;	
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			facePoints[i][j].x = baseFacePoints[i][j].x;
+			facePoints[i][j].y = baseFacePoints[i][j].y;
+			facePoints[i][j].z = baseFacePoints[i][j].z;
+		}
+	}
+}
+
+/*
+ * 
+ */
+void Cubie::ResetVisibleFace()
+{
+	if (blackFaceToReset != -1)
+	{
+		facesShown[faceInPos[blackFaceToReset]] = false;
+		blackFaceToReset = -1;
+	}
+}
+
+/* Called when a rotation is made. cubies must make an inner black face
+ * visible while rotation is in motion (which must be invisible once it's complete)
+ */
+void Cubie::SetFacePositionVisible(bool toggle, int position)
+{
+	facesShown[faceInPos[position]] = toggle;
+	blackFaceToReset = position;
+}
+
+/*===============================================================================================================================================================================
+ * 			RCSTATE CLASS
+ ===============================================================================================================================================================================*/
+
+/*
+ * 
+ */
+void RCState::RotateFace(int face, int move)
+{
+	// Move the cubes
+	if (move != 2)
+	{
+		bool forward;
+		if (move == 0) forward = true;
+		else forward = false;
+		
+		ShiftPositions(edgesOnFace[face], forward);
+		ShiftPositions(cornersOnFace[face], forward);
+	} else //180* turn
+	{
+		SwapPositions(edgesOnFace[face][0], edgesOnFace[face][2]);
+		SwapPositions(edgesOnFace[face][1], edgesOnFace[face][3]);
+		SwapPositions(cornersOnFace[face][0], cornersOnFace[face][2]);
+		SwapPositions(cornersOnFace[face][1], cornersOnFace[face][3]);
+	}
+	
+	//Rotate the Cubes
+	RotateEdges(face, move);
+	RotateCorners(face, move);
+}
+
+/*
+ * 
+ */
+void RCState::RotateEdges(int face, int move)
+{
+	if (move != 2 && (face == 1 || face == 3))
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			rotation[edgesOnFace[face][i]] = 1 - rotation[edgesOnFace[face][i]];
+		}
+	}
+}
+
+/*
+ * 
+ */
+void RCState::RotateCorners(int face, int move)
+{
+	if (move != 2)
+	{
+		int index;
+		if (face % 5 == 0) index = 0;
+		else if (face % 2 == 1) index = 1;
+		else index = 2;
+		
+		if (index != 0)
+		{
+			for (int i = 0; i < 4; i++)
+			{
+				int rot = rotation[cornersOnFace[face][i]] + index;
+				if (rot > 2) rot -= 3;
+				rotation[cornersOnFace[face][i]] = rot;
+			}
+		}
+	}
+}
+
+/*
+ * 
+ */
+void RCState::SwapPositions(int p1, int p2)
+{
+	int temp = indices[p1];
+	indices[p1] = indices[p2];
+	indices[p2] = temp;
+}
+
+/*
+ * 
+ */
+void RCState::ShiftPositions(int (&arr)[4], bool forward)
+{
+	int temp;
+	if (forward) // clockwise
+	{
+		temp = indices[arr[3]];
+		for (int i = 3; i > 0; i--)
+		{
+			indices[arr[i]] = indices[arr[i-1]];
+		}
+		indices[0] = temp;
+	} else //CCW
+	{
+		temp = indices[arr[0]];
+		for (int i = 0; i < 3; i++)
+		{
+			indices[arr[i]] = indices[arr[i+1]];
+		}
+		indices[3] = temp;
+	}
+}
+
+/*===============================================================================================================================================================================
+ * 			RC CLASS
+ ===============================================================================================================================================================================*/
+
+/*
+ * 
+ */
+void RC::DrawCubies(Graphics::Display &display)
+{
+	if (rotating)
+	{
+		DrawCubiesRotating(display);
+	} else
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			cubies[i].Draw(display);
+		}
+	}
+}
+
+/*
+ * 
+ */
+void RC::DrawCubiesRotating(Graphics::Display &display)
+{
+	bool jumped = false;
+	if (rotatingFaceBehind)
+	{
+		 goto drawFace;
+	} else goto drawCubie;
+	
+	drawFace:
+		for (int i = 0; i < 9; i++)
+			cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].Draw(display);	
+		if (!jumped)
+		{
+			jumped = true;
+			goto drawCubie;
+		}
+		return;
+	drawCubie:
+		for (int i = 0; i < 17; i++)
+			cubies[cubieInPos[notInFaceTurning[faceTurning][i]]].Draw(display);
+		if (!jumped)
+		{
+			jumped = true;
+			goto drawFace;
+		}
+		return;
+}
+
+/*
+ * 
+ */
+void RC::RotateCubies(float add[3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		rotationTotal[i] += add[i];
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		cubies[i].ResetToBase();
+		cubies[i].RotateRelative(rotationTotal);
+	}
+}
+
+/*
+ * 
+ */
+void RC::RotateFace(int face, int move)
+{
+	if (!rotating)
+	{
+		bool clockwise = true;
+		int axis;
+		turnArr[0] = 0;
+		turnArr[1] = 0;
+		turnArr[2] = 0;
+		rotating = true;
+		faceTurning = face;
+		rotProgress = 0;
+		
+		float add = piOver2;
+		if (face == 0 || face == 3 || face == 4)
+		{
+			add *= -1;
+		}
+		if (face == 5 || face == 3 || face == 4 )
+		{
+			clockwise = !clockwise;
+		}
+		
+		// Move cubies on face
+		int temp, temp2;
+		
+		if (move == 0) //CW
+		{
+			temp = cubieInPos[cubiesOnFace[face][edgeOrder[3]]];
+			temp2 = cubieInPos[cubiesOnFace[face][cornerOrder[3]]];
+			for (int i = 3; i > 0; i--)
+			{
+				cubieInPos[cubiesOnFace[face][edgeOrder[i]]] = cubieInPos[cubiesOnFace[face][edgeOrder[i-1]]];
+				cubieInPos[cubiesOnFace[face][cornerOrder[i]]] = cubieInPos[cubiesOnFace[face][cornerOrder[i-1]]];
+			}
+			cubieInPos[cubiesOnFace[face][edgeOrder[0]]] = temp;
+			cubieInPos[cubiesOnFace[face][cornerOrder[0]]] = temp2;
+		} else if (move == 1) //CCW
+		{
+			add *= -1;
+			
+			temp = cubieInPos[cubiesOnFace[face][edgeOrder[0]]];
+			temp2 = cubieInPos[cubiesOnFace[face][cornerOrder[0]]];
+			for (int i = 0; i < 3; i++)
+			{
+				cubieInPos[cubiesOnFace[face][edgeOrder[i]]] = cubieInPos[cubiesOnFace[face][edgeOrder[i+1]]];
+				cubieInPos[cubiesOnFace[face][cornerOrder[i]]] = cubieInPos[cubiesOnFace[face][cornerOrder[i+1]]];
+			}
+			cubieInPos[cubiesOnFace[face][edgeOrder[3]]] = temp;
+			cubieInPos[cubiesOnFace[face][cornerOrder[3]]] = temp2;
+			
+			clockwise = !clockwise;
+		} else //180
+		{
+			add *= 2;
+			
+			for (int i = 0; i < 2; i++)
+			{
+				temp = cubieInPos[cubiesOnFace[face][edgeOrder[i]]];
+				temp2 = cubieInPos[cubiesOnFace[face][cornerOrder[i]]];
+				cubieInPos[cubiesOnFace[face][edgeOrder[i]]] = cubieInPos[cubiesOnFace[face][edgeOrder[i+2]]];
+				cubieInPos[cubiesOnFace[face][cornerOrder[i]]] = cubieInPos[cubiesOnFace[face][cornerOrder[i+2]]];
+				cubieInPos[cubiesOnFace[face][edgeOrder[i+2]]] = temp;
+				cubieInPos[cubiesOnFace[face][cornerOrder[i+2]]] = temp2;
+			}
+		}
+		//Set direction and distance of turn
+		if (face%5 == 0) {
+			turnArr[1] = add;
+			axis = 1;
+		} else if (face == 1 || face == 3) {
+			turnArr[2] = add;
+			axis = 2;
+		} else {
+			turnArr[0] = add;
+			axis = 0;
+		}
+		for (int i = 0; i < 9; i++)
+		{
+			//Edit the base to be FULLY rotated
+			cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].RotateBase(turnArr);
+			
+			//Rotate Cubie face positions
+			cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].RotateFacePos(clockwise, axis);
+			if (move == 2) cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].RotateFacePos(clockwise, axis);
+			
+			//Set black face to be visible on each rotated cubie
+			cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].SetFacePositionVisible(true, faceBlackUnderside[faceTurning]);
+			
+			//Set black face to be visible on each cubie now exposed by the turn face rotating
+			int toEdit = cubiesOnFace[faceTurning][i];
+			if (toEdit >= 13) toEdit++;
+			toEdit += fromFaceToCenter[faceTurning];
+			if (toEdit != 13)
+			{
+				if (toEdit >= 14) toEdit--;
+				cubies[cubieInPos[toEdit]].SetFacePositionVisible(true, faceTurning);
+			}
+		}
+	}
+}
+
+/*
+ * 
+ */
+void RC::InterpFaceRot(float progress)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		//Inbetween val
+		interpArr[i] = turnArr[i] * (1-progress) * -1;
+	}
+	// Only turns the 9 face cubies
+	for (int i = 0; i < 9; i++) 
+	{
+		//Reset Rotation
+		cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].ResetToBase();
+		//Apply interpolated face rotation to straight-on cubie
+		cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].RotateRelative(interpArr);
+		//Apply total rotation back
+		cubies[cubieInPos[cubiesOnFace[faceTurning][i]]].RotateRelative(rotationTotal);
+	}
+	//Turn the rest of the cubie to match
+	for (int i = 0; i < 17; i++)
+	{
+		cubies[cubieInPos[notInFaceTurning[faceTurning][i]]].ResetToBase();
+		cubies[cubieInPos[notInFaceTurning[faceTurning][i]]].RotateRelative(rotationTotal);
+	}
+	
+	//Determing whether the center cubie of the face is behind a z of 0
+	float centerCubieZ = (cubies[cubieInPos[cubiesOnFace[faceTurning][4]]].points[0].z - 
+			cubies[cubieInPos[cubiesOnFace[faceTurning][4]]].points[6].z)/2 + 
+			cubies[cubieInPos[cubiesOnFace[faceTurning][4]]].points[6].z;
+	rotatingFaceBehind = (centerCubieZ >= 0); //if it is behind (larger than) 0, it is drawn second	
+}
+
+/*
+ * 
+ */
+void RC::TestUpdate()
+{
+	float testRot [3];
+	if (passiveRot)
+		for (int i = 0; i < 3; i++)
+		{
+			//if (i == 1)
+			testRot[i] = 0.03f;
+		}
+	
+	if (rotating)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			rotationTotal[i] += testRot[i];
+		}
+		
+		rotProgress += turnSpd;
+		if (rotProgress > 1)
+		{
+			rotating = false;
+			rotProgress = 1;
+			for (int i = 0; i < 26; i++)
+			{
+				cubies[i].ResetVisibleFace();
+			}
+		}
+		InterpFaceRot(rotProgress);
+	} else
+	{
+		RotateCubies(testRot);
+	}
+}
+
 void RC::GetSuccessors(const RCState &nodeID, std::vector<RCState> &neighbors) const
 {
 	neighbors.resize(18);
@@ -533,4 +1210,93 @@ void RC::OpenGLDraw(const RCState&, const RCAction&) const
 // Draw a RCState. Internally do the 3d transform for the drawing.
 void RC::Draw(Graphics::Display &display, const RCState&) const
 {
+}
+
+
+
+/*
+ * 
+ */
+void EditPointMult(float (&vec)[3][3], Graphics::point & p)
+{
+	int vecCols = 3;
+	int vecRows = 3;
+
+	array3[0] = p.x;
+	array3[1] = p.y;
+	array3[2] = p.z;
+
+	for (int i = 0; i < vecRows; i++)
+	{
+		float sum = 0.f;
+
+		for (int j = 0; j < vecCols; j++)
+		{
+			sum += vec[i][j] * array3[j];
+		}
+
+		switch (i)
+		{
+			case 0: p.x = sum;
+				break;
+			case 1: p.y = sum;
+				break;
+			case 2: p.z = sum;
+				break;
+		}
+	}
+}
+
+/* Edit an existing 3x3 vector to become the
+ * X rotation multiplication matrix for a given angle
+ */
+//void MakeMatrixRotX(std::vector<std::vector<float>> & a, float angle) {
+void MakeMatrixRotX(float (&a)[3][3], float angle) {
+	a[0][0] = 1;
+	a[0][1] = 0;
+	a[0][2] = 0;
+
+	a[1][0] = 0;
+	a[1][1] = cos(angle);
+	a[1][2] = -sin(angle);
+
+	a[2][0] = 0;
+	a[2][1] = sin(angle);
+	a[2][2] = cos(angle);
+}
+
+/* Edit an existing 3x3 vector to become the
+ * Y rotation multiplication matrix for a given angle
+ */
+//void MakeMatrixRotY(std::vector<std::vector<float>> & a, float angle) {
+void MakeMatrixRotY(float (&a)[3][3], float angle) {	
+	a[0][0] = cosf(angle);
+	a[0][1] = 0;
+	a[0][2] = -sin(angle);
+
+	a[1][0] = 0;
+	a[1][1] = 1;
+	a[1][2] = 0;
+
+	a[2][0] = sin(angle);
+	a[2][1] = 0;
+	a[2][2] = cos(angle);
+}
+
+/* Edit an existing 3x3 vector to become the
+ * Z rotation multiplication matrix for a given angle
+ */
+//void MakeMatrixRotZ(std::vector<std::vector<float>> & a, float angle) {
+void MakeMatrixRotZ(float (&a)[3][3], float angle) {
+	a[0][0] = cosf(angle);
+	a[0][1] = -sin(angle);
+	a[0][2] = 0;
+
+	a[1][0] = sin(angle);
+	a[1][1] = cos(angle);
+	a[1][2] = 0;
+
+	a[2][0] = 0;
+	a[2][1] = 0;
+	a[2][2] = 1;
 }
--- environments/RC.h
+++ environments/RC.h
@@ -14,6 +14,79 @@
 #include "SearchEnvironment.h"
 #include "PDBHeuristic.h"
 
+/*===============================================================================================================================================================================
+ * 			CUBIE CLASS
+ ===============================================================================================================================================================================*/
+
+/* Contains all data necessary to DRAW a cubie
+ * Can draw itself
+ */
+
+class Cubie
+{
+public:
+	//Changing Variables
+	std::vector<Graphics::point> basePoints;
+	std::vector<std::vector<Graphics::point>> baseFacePoints;
+	std::vector<Graphics::point> points;
+	std::vector<std::vector<Graphics::point>> facePoints;
+	rgbColor faceCols [6];
+	int index;
+	bool facesShown [6];
+	int blackFaceToReset = -1;
+	int faceInPos [6] = {0,1,2,3,4,5};
+
+	//Constants
+	const float sideLen = 0.2f;
+	const float faceSize = 0.85f;
+	const rgbColor outCol = rgbColor(0.f, 0.f, 0.f);
+	const rgbColor inCol = rgbColor(0.04f, 0.04f, 0.06f);
+	const std::vector<std::vector<float>> projection =
+	{
+		{1, 0, 0},
+		{0, 1, 0}
+	};
+	const int pointsOnFace [6][4] =
+	{
+		{0, 1, 5, 4},
+		{0, 1, 2, 3},
+		{3, 0, 4, 7},
+		{4, 5, 6, 7},
+		{1, 2, 6, 5},
+		{2, 3, 7, 6}
+	};
+	const rgbColor faceColors [6] =
+	{
+		rgbColor(255.f, 255.f, 255.f),	//	sf::Color::White,
+		rgbColor(0.f, 0.f, 255.f),		//	sf::Color::Blue,
+		rgbColor(255.f, 0.f, 0.f),		//	sf::Color::Red,
+		rgbColor(0.f, 255.f, 0.f),		//	sf::Color::Green,
+		Colors::orange, //rgbColor(255.f, 89.f, 0.f),		//	orangeCol,
+		Colors::yellow //rgbColor(255.f, 240.f, 31.f),	//	sf::Color::Yellow,
+	};
+	const int faceOrderByAxis [3][4] =
+	{
+		{0,1,5,3},
+		{1,2,3,4},
+		{0,4,5,2}
+	};
+	
+	void Initialize(int ind);
+	void Draw(Graphics::Display &display);
+	void DrawFace(Graphics::Display &display, int index);
+	void RotateRelative(float angle [3]);
+	void RotateBase(float angle [3]);
+	void RotateFacePos(bool clockwise, int axis); 
+	void ResetToBase();
+	void ResetVisibleFace();
+	void SetFacePositionVisible(bool toggle, int position);
+	//Cubie & operator=(const Cubie & rhs);
+};
+
+/*===============================================================================================================================================================================
+ * 			RCSTATE CLASS
+ ===============================================================================================================================================================================*/
+
 class RCState
 {
 public:
@@ -24,8 +97,40 @@ public:
 	void Reset()
 	{
 		// Set all values to default
+		for (int i = 0; i < 20; i++)
+		{
+			indices[i] = i;
+			rotation[i] = 0;
+		}
 	}
 	// Put data structures here
+	int indices[20]; //Index of cubie in position
+	int rotation[20]; //Rotation of cubie in position
+	
+	int edgesOnFace[6][4] = 
+	{
+		{0, 6, 4, 2},  //0
+		{0, 1, 8, 7},  //1
+		{2, 3, 9, 1},  //2
+		{4, 5, 10, 3}, //3
+		{6, 7, 11, 5}, //4
+		{9, 10, 11, 8} //5
+	};
+	int cornersOnFace[6][4] = 
+	{
+		{4 +11, 3 +11, 2 +11, 1 +11}, //0
+		{4 +11, 1 +11, 5 +11, 8 +11}, //1
+		{1 +11, 2 +11, 6 +11, 5 +11}, //2
+		{2 +11, 3 +11, 7 +11, 6 +11}, //3
+		{3 +11, 4 +11, 8 +11, 7 +11}, //4
+		{5 +11, 6 +11, 7 +11, 8 +11}  //5
+	};
+	
+	void RotateFace(int face, int move);
+	void RotateEdges(int face, int move);
+	void RotateCorners(int face, int move);
+	void SwapPositions(int p1, int p2);
+	void ShiftPositions(int (&arr)[4], bool forward);
 };
 
 typedef int RCAction;
@@ -48,9 +153,104 @@ static bool operator==(const RCState &l1, const RCState &l2)
 //	return out;
 //}
 
+//void drawCube(Graphics::Display &display, int xPos, int yPos, int zPos, float tempAngle);
+
+/*===============================================================================================================================================================================
+ * 			RC CLASS
+ ===============================================================================================================================================================================*/
+
 class RC : public SearchEnvironment<RCState, RCAction>
 {
 public:
+	//Constants
+	const int fromFaceToCenter[6] =
+	{
+		3, 9, 1, -9, -1, -3
+	};
+	const int faceBlackUnderside[6] =
+	{
+		5, 3, 4, 1, 2, 0
+	};
+	const float piOver2 = 1.57079632679;
+	const float turnSpd = 0.04;
+	const int edgeOrder [4] = {1, 5, 7, 3};
+	const int cornerOrder [4] = {0, 2, 8, 6};
+	
+	//Cubie Object items
+	Cubie cubies [26];
+	float rotationTotal [3];
+	int cubiesOnFace [6][9]; //Face, position
+	int cubieInPos[26];
+	int faceTurning;
+	int notInFaceTurning[6][17];
+	bool rotating = false; 
+	bool rotatingFaceBehind = false;
+	float rotProgress = 0;
+	float turnArr[3];
+	float interpArr[3];
+	bool passiveRot = true;
+	
+	void DrawCubies(Graphics::Display &display);
+	void DrawCubiesRotating(Graphics::Display &display);
+	void RotateCubies(float add[3]);
+	void RotateFace(int face, int move);
+	void InterpFaceRot(float progress);
+	void TestUpdate();
+
+	RC()
+	{
+		for (int i = 0; i < 26; i++)
+		{
+			//std::cout << cubies[i].a << '\n';
+			cubies[i].Initialize(i);
+			cubieInPos[i] = i;
+		}
+		for (int i = 0; i < 3; i++)
+		{
+			rotationTotal[i] = 0.f;
+		}
+		// Initialize cubiesOnFace
+		for (int i = 0; i < 9; i++)
+		{
+			int in = (i/3)*-9 + (i%3) + 18; in -= in/14;
+			cubiesOnFace[0][i] = in;
+			in = (i/3)*9 + (i%3) + 6; in -= in/14;
+			cubiesOnFace[5][i] = in;
+			
+			in = (i/3)*3 + (i%3)*-9 + 18; in -= in/14;
+			cubiesOnFace[2][i] = in;
+			in = (i/3)*3 + (i%3)*9 + 2; in -= in/14;
+			cubiesOnFace[4][i] = in;
+			
+			cubiesOnFace[1][i] = i;
+			in = (i/3)*3 - (i%3) + 20; in -= in/14;
+			cubiesOnFace[3][i] = in;
+		}
+		//Initialize notInFaceTurning
+		for (int i = 0; i < 6; i++)
+		{
+			int j = 0;
+			for (int k =  0; k < 26; k++)
+			{
+				bool add = true;
+				for (int l = 0; l < 9; l++)
+				{
+					if (cubiesOnFace[i][l] == k)
+					{
+						//Don't add it to the list
+						add = false;
+						break;
+					}
+				}
+				if (add)
+				{
+					notInFaceTurning[i][j] = k; 
+					j++;
+				}
+			}
+		}
+	}
+
 	std::string GetName() { return "RC"; }
 //	void SetPruneSuccessors(bool val) { pruneSuccessors = val; history.resize(0); }
 	virtual void GetSuccessors(const RCState &nodeID, std::vector<RCState> &neighbors) const;
