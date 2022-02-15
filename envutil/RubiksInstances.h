a3e178383aabf60758a08c5ee950efe52c7bf182
Nathan Sturtevant <nathanst@gmail.com>
2022-02-15 12:53:21 -0700

    Added support for getting instances in new RC implementation

--- envutil/RubiksInstances.h
+++ envutil/RubiksInstances.h
@@ -10,13 +10,20 @@
 
 #include <stdio.h>
 #include "RubiksCube.h"
+#include "RC.h"
 
 namespace RubiksCubeInstances {
-	void GetKorfRubikInstance(RubiksState &start, int which);
 
-	void GetSuperFlip(RubiksState &start);
-	void GetDepth20(RubiksState &start, int which);
-	void GetRandomN(RubiksState &start, int N, int which);
+void GetKorfRubikInstance(RubiksState &start, int which);
+void GetKorfRubikInstance(RCState &start, int which);
+
+void GetSuperFlip(RubiksState &start);
+void GetSuperFlip(RCState &start);
+
+void GetDepth20(RubiksState &start, int which);
+
+void GetRandomN(RubiksState &start, int N, int which);
+void GetRandomN(RCState &start, int N, int which);
 
 }
 
